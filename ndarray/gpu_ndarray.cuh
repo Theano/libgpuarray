cfdf7b71342066494b77ebfc189e03bfd17d4831
Frederic <nouiz@nouiz.org>
2011-05-25 14:44:06 -0400

    use ssize_t instead of npy_intp in code without python dependency. They should be equivalent type.

--- ndarray/gpu_ndarray.cuh
+++ ndarray/gpu_ndarray.cuh
@@ -13,8 +13,8 @@ typedef struct GpuNdArray{
    *  This pointer is never followed, except during Py_DECREF when we do not need it any longer.
    */
   void * base;
-  npy_intp * dimensions; //dim0, dim1, ... dim nd
-  npy_intp * strides; //stride0, stride1, ... stride nd
+  ssize_t  * dimensions; //dim0, dim1, ... dim nd
+  ssize_t * strides; //stride0, stride1, ... stride nd
   int flags; // Flags, see numpy flags
   //DTYPE dtype; // fine for numeric types
   //DtypeMeta * dtype_meta; // reserved for future use.
