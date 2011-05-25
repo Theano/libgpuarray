ca19114fedad01e1c4f0939a18ba74f36a58b637
Frederic <nouiz@nouiz.org>
2011-05-25 14:40:06 -0400

    code clean up.

--- ndarray/pygpu_language.h
+++ ndarray/pygpu_language.h
@@ -41,57 +41,5 @@ outstanding_mallocs(PyObject* self, PyObject * args)
 {
     return PyInt_FromLong(_outstanding_mallocs[0]);
 }
-/*
-/////////////////////////////
-// Satisfying reqs to be Type
-/////////////////////////////
 
-//DON'T use directly(if their is other PyGpuNdArrayObject that point to it, it will cause problem)! use Py_DECREF() instead
-static void
-PyGpuNdArrayObject_dealloc(PyGpuNdArrayObject* self)
-{
-    if (0) fprintf(stderr, "PyGpuNdArrayObject_dealloc\n");
-    if (0) std::cerr << "PyGpuNdArrayObject dealloc " << self << '\n';
-    if (0) std::cerr << "PyGpuNdArrayObject dealloc " << self << " " << PyGpuNdArray_DATA(self) << '\n';
-
-    if(self->ob_refcnt>1)
-      printf("WARNING:PyGpuNdArrayObject_dealloc called when their is still active reference to it.\n");
-
-    if (self->data_allocated){
-        assert(PyGpuNdArray_DATA(self));
-        if (PyGpuNdArray_DATA(self)){
-            if (device_free(PyGpuNdArray_DATA(self))){
-	      fprintf(stderr,
-		  "!!!! error freeing device memory %p (self=%p)\n",
-		  PyGpuNdArray_DATA(self), self);
-	    }
-	    PyGpuNdArray_DATA(self) = NULL;
-	}
-    }
-    PyGpuNdArray_OFFSET(self) = 0;
-    PyGpuNdArray_NDIM(self) = -1;
-    Py_XDECREF(self->base);
-    self->base = NULL;
-    if (PyGpuNdArray_DIMS(self)){
-        free(PyGpuNdArray_DIMS(self));
-        PyGpuNdArray_DIMS(self) = NULL;
-    }
-    if (PyGpuNdArray_STRIDES(self)){
-        free(PyGpuNdArray_STRIDES(self));
-        PyGpuNdArray_STRIDES(self) = NULL;
-    }
-    PyGpuNdArray_FLAGS(self) = NPY_DEFAULT;
-    Py_XDECREF(self->descr);//TODO: How to handle the refcont on this object?
-    self->descr = NULL;
-    self->data_allocated = 0;
-
-    self->ob_type->tp_free((PyObject*)self);
-    --_outstanding_mallocs[1];
-    if(0){
-        fprintf(stderr, "device_malloc_counts: (device) %i (obj) %i\n",
-                _outstanding_mallocs[0],
-                _outstanding_mallocs[1]);
-    }
-}
-*/
 #endif
