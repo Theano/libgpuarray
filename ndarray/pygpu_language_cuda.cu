f0bc8e113e32513f5cacf7283bb364bb7406d5ed
Frederic <nouiz@nouiz.org>
2011-05-26 14:15:50 -0400

    Added comment to make vim/emacs respect the coding style.

--- ndarray/pygpu_language_cuda.cu
+++ ndarray/pygpu_language_cuda.cu
@@ -89,3 +89,14 @@ int device_free(void *ptr)
 
 
 
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
