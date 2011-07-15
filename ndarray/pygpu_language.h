2a154e2ea5f42460a54fd31a5f153650e1d939e1
Frederic <nouiz@nouiz.org>
2011-07-15 15:38:19 -0400

    moved PyGpuNdArray_alloc_contiguous to pygpu_language.py
    This will be needed to localise in pygpu_language.py all CUDA related code.

--- ndarray/pygpu_language.h
+++ ndarray/pygpu_language.h
@@ -5,7 +5,7 @@
 #define _PYGPU_LANGUAGE_H
 #include <Python.h>
 //#include <iostream>
-//#include <pygpu_ndarray.cuh>
+#include <pygpu_ndarray_object.h>
 
 #ifdef __DEVICE_EMULATION__
 #define NUM_VECTOR_OP_BLOCKS                4096
@@ -64,5 +64,108 @@ outstanding_mallocs(PyObject* self, PyObject * args)
 }
 
 int PyGpuNdArray_CopyFromPyGpuNdArray(PyGpuNdArrayObject * self, PyGpuNdArrayObject * other, bool unbroadcast = false);
+
+/**
+ * PyGpuNdArray_alloc_contiguous
+ *
+ * Allocate storage space for a tensor of rank 'nd' and given dimensions.
+ *
+ * Note: PyGpuNdArray_alloc_contiguous is templated to work for both int dimensions and npy_intp dimensions
+ */
+template<typename inttype>
+int PyGpuNdArray_alloc_contiguous(PyGpuNdArrayObject *self, const int nd, const inttype * dim, NPY_ORDER order=NPY_CORDER)
+{
+    int verbose = 0;
+    if (verbose) fprintf(stderr, "PyGpuNdArray_alloc_contiguous: start nd=%i\n descr=%p", nd);
+
+    if (!PyGpuNdArray_DESCR(self)){
+        PyErr_SetString(PyExc_ValueError,
+                        "PyGpuNdArray_alloc_contiguous: The array don't have a type! We can't allocate it!\n");
+        return -1;
+    }
+    
+    // allocate an empty ndarray with c_contiguous access
+    // return 0 on success
+    int size = 1; //set up the strides for contiguous tensor
+    assert (nd >= 0);
+    if (PyGpuNdArray_set_nd(self, nd))
+    {
+        return -1;
+    }
+    //TODO: check if by any chance our current dims are correct,
+    //      and strides already contiguous
+    //      in that case we can return right here.
+    if (verbose) fprintf(stderr, "PyGpuNdArray_alloc_contiguous: before itemsize descr=%p elsize=%i\n", self->descr, self->descr->elsize);
+    int elsize = PyGpuNdArray_ITEMSIZE((PyObject*)self);
+    if (verbose) fprintf(stderr, "PyGpuNdArray_alloc_contiguous: set_nd! elsize=%i\n", nd,elsize);
+    if(order != NPY_FORTRANORDER){
+      if (verbose) fprintf(stderr, "PyGpuNdArray_alloc_contiguous: NPY_CORDER\n");
+      for (int i = nd-1; i >= 0; --i){
+        PyGpuNdArray_STRIDE(self,i) = size * elsize;
+        PyGpuNdArray_DIM(self,i) = dim[i];
+        size = size * dim[i];
+      }
+    }else{
+      if (verbose) fprintf(stderr, "PyGpuNdArray_alloc_contiguous: NPY_FORTRANORDER\n");
+      size = dim[nd-1];
+      PyGpuNdArray_STRIDE(self, 0) = elsize;
+      PyGpuNdArray_DIM(self, nd-1) = dim[nd-1];
+      for (int i = 1; i < nd; ++i){
+        PyGpuNdArray_STRIDE(self, i) = PyGpuNdArray_STRIDE(self, i-1) * dim[i-1];
+        PyGpuNdArray_DIM(self, nd-i-1) = dim[nd-i-1];
+        size = size * dim[i];
+      }
+    }
+
+    if (self->data_allocated != size)
+    {
+        if (device_free(PyGpuNdArray_DATA(self)))
+        {
+            // Does this ever happen??  Do we need to set data_allocated or data to 0?
+            return -1;
+        }
+        assert(size>0);
+	if (verbose) fprintf(stderr, "PyGpuNdArray_alloc_contiguous: will allocate for size=%d elements\n", size);
+
+        PyGpuNdArray_DATA(self) = (char*)device_malloc(size * PyGpuNdArray_ITEMSIZE((PyObject *)self));
+        if (!PyGpuNdArray_DATA(self))
+        {
+            PyGpuNdArray_set_nd(self,-1);
+            self->data_allocated = 0;
+            PyGpuNdArray_DATA(self) = 0;
+            return -1;
+        }
+
+        self->data_allocated = size;
+	self->gpu_ndarray.flags = NPY_DEFAULT;
+	PyGpuNdArray_FLAGS(self) |= NPY_WRITEABLE;
+	PyGpuNdArray_FLAGS(self) |= NPY_OWNDATA;
+	if(nd == 0){
+	  PyGpuNdArray_FLAGS(self) &= ~NPY_F_CONTIGUOUS;
+	  PyGpuNdArray_FLAGS(self) |= NPY_C_CONTIGUOUS;
+	}else if(nd == 1){//set c and f contiguous
+	  PyGpuNdArray_FLAGS(self) |= NPY_F_CONTIGUOUS;
+	  PyGpuNdArray_FLAGS(self) |= NPY_C_CONTIGUOUS;	  
+	}else if(order != NPY_FORTRANORDER){//set c contiguous
+	  PyGpuNdArray_FLAGS(self) &= ~NPY_F_CONTIGUOUS;
+	  PyGpuNdArray_FLAGS(self) |= NPY_C_CONTIGUOUS;
+	}else{//set f contiguous
+	  PyGpuNdArray_FLAGS(self) |= NPY_F_CONTIGUOUS;
+	  PyGpuNdArray_FLAGS(self) &= ~NPY_C_CONTIGUOUS;
+	}
+	PyGpuNdArray_FLAGS(self) &= ~NPY_UPDATEIFCOPY;
+    }else{
+      assert(0);// How to check for the flags? Need to check if already contiguous.
+    }
+
+    if (order != NPY_FORTRANORDER) {
+        assert(PyGpuNdArray_is_c_contiguous(self));
+    } else {
+        assert(PyGpuNdArray_is_f_contiguous(self));
+    }
+    if (verbose) fprintf(stderr, "PyGpuNdArray_alloc_contiguous: end\n");
+    return 0;
+}
+
 
 #endif
