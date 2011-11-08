71b986f6b1f0b400b4125039c18c4f8f3fb3bc35
Frederic <nouiz@nouiz.org>
2011-11-08 13:43:21 -0500

    Test the new version of code that support offset.

--- ndarray/test_gpu_ndarray.py
+++ ndarray/test_gpu_ndarray.py
@@ -38,18 +38,35 @@ def check_all(x, y):
     check_meta(x, y)
     assert numpy.allclose(numpy.asarray(x), numpy.asarray(y))
 
-def test_transfer():
-    for shp in [(5,),(6,7),(4,8,9),(1,8,9)]:
-        for dtype in dtypes_all:
-            a = numpy.random.rand(*shp) * 10
-            b = gpu_ndarray.GpuNdArrayObject(a)
-            c = numpy.asarray(b)
 
-            assert numpy.allclose(c,a)
-            assert a.shape == b.shape == c.shape
-            assert a.strides == b.strides == c.strides
-            assert a.dtype == b.dtype == c.dtype
-            assert c.flags.c_contiguous
+def gen_gpu_nd_array(shape, dtype='float32', offseted=False):
+    if offseted and len(shape) > 0:
+        a = numpy.random.rand(shape[0] + 1, *shape[1:]) * 10
+        a = numpy.asarray(a, dtype=dtype)
+        b = gpu_ndarray.GpuNdArrayObject(a)
+        b = b[1:]
+        a = a[1:]
+        assert b.offset != 0
+    else:
+        a = numpy.random.rand(*shape) * 10
+        a = numpy.asarray(a, dtype=dtype)
+        b = gpu_ndarray.GpuNdArrayObject(a)
+    return a, b
+
+
+def test_transfer():
+    for shp in [(), (5,), (6, 7), (4, 8, 9), (1, 8, 9)]:
+        for dtype in dtypes_all:
+            for offseted in [True, False]:
+                a, b = gen_gpu_nd_array(shp, dtype, offseted)
+                c = numpy.asarray(b)
+
+                assert numpy.allclose(c, a)
+                assert a.shape == b.shape == c.shape
+                assert a.strides == b.strides == c.strides
+                assert a.dtype == b.dtype == c.dtype == dtype
+                assert c.flags.c_contiguous
+
 
 def test_transfer_not_contiguous():
     """
@@ -109,11 +126,11 @@ def test_empty():
     for shp in [(), (5,),(6,7),(4,8,9),(1,8,9)]:
         for order in ["C", "F"]:
             for dtype in dtypes_all:
-                x = numpy.zeros(shp, dtype, order)
+                x = numpy.empty(shp, dtype, order)
                 y = gpu_ndarray.empty(shp, dtype, order)
                 check_meta(x, y)
-    x = gpu_ndarray.zeros(())# no dtype and order param
-    y = numpy.zeros(())
+    x = gpu_ndarray.empty(())# no dtype and order param
+    y = numpy.empty(())
     check_meta(x, y)
     try:
         gpu_ndarray.empty()
@@ -126,15 +143,15 @@ def test_empty():
 def test_mapping_getitem_ellipsis():
     for shp in [(5,),(6,7),(4,8,9),(1,8,9)]:
         for dtype in dtypes_all:
-            a = numpy.asarray(numpy.random.rand(*shp), dtype=dtype)
-            a_gpu = gpu_ndarray.GpuNdArrayObject(a)
+            for offseted in [True, False]:
+                a, a_gpu = gen_gpu_nd_array(shp, dtype, offseted)
 
-            b = a_gpu[...]
-            assert b.bytes == a_gpu.bytes
-            assert b.strides == a.strides
-            assert b.shape == a.shape
-            b_cpu = numpy.asarray(b)
-            assert numpy.allclose(a, b_cpu)
+                b = a_gpu[...]
+                assert b.bytes == a_gpu.bytes
+                assert b.strides == a.strides
+                assert b.shape == a.shape
+                b_cpu = numpy.asarray(b)
+                assert numpy.allclose(a, b_cpu)
 
 def test_copy_view():
     from ..array import may_share_memory
@@ -144,50 +161,55 @@ def test_copy_view():
         if a_op.base is None:
             assert b_op.base is None
         else:
-            assert a_op.base is a and b_op.base is b
+            assert a_op.base is a
+            if b.base:
+                # We avoid having a series of object connected by base.
+                # This is to don't bloc the garbage collection.
+                assert b_op.base is b.base
+            else:
+                assert b_op.base is b
 
     for shp in [(5,),(6,7),(4,8,9),(1,8,9)]:
         for dtype in dtypes_all:
-            #TODO test copy unbroadcast!
-            shape = (5,)
-            a = numpy.asarray(numpy.random.rand(*shape), dtype='float32')
+            for offseted in [False, True]:
+                #TODO test copy unbroadcast!
+                a, b = gen_gpu_nd_array(shp, dtype, offseted)
 
-            b = gpu_ndarray.GpuNdArrayObject(a)
-            assert numpy.allclose(a, numpy.asarray(b))
-            check_flags(a, b)
+                assert numpy.allclose(a, numpy.asarray(b))
+                check_flags(a, b)
 
-            c = b.copy()
-            assert numpy.allclose(a, numpy.asarray(c))
-            check_flags(c, a.copy())
-            check_memory_region(a, a.copy(), b, c)
+                c = b.copy()
+                assert numpy.allclose(a, numpy.asarray(c))
+                check_flags(c, a.copy())
+                check_memory_region(a, a.copy(), b, c)
 
-            d = copy.copy(b)
-            assert numpy.allclose(a, numpy.asarray(d))
-            check_flags(d, copy.copy(a))
-            check_memory_region(a, copy.copy(a), b, d)
+                d = copy.copy(b)
+                assert numpy.allclose(a, numpy.asarray(d))
+                check_flags(d, copy.copy(a))
+                check_memory_region(a, copy.copy(a), b, d)
 
-            e = b.view()
-            assert numpy.allclose(a, numpy.asarray(e))
-            check_flags(e, a.view())
-            check_memory_region(a, a.view(), b, e)
+                e = b.view()
+                assert numpy.allclose(a, numpy.asarray(e))
+                check_flags(e, a.view())
+                check_memory_region(a, a.view(), b, e)
 
-            f = copy.deepcopy(b)
-            assert numpy.allclose(a, numpy.asarray(f))
-            check_flags(f, copy.deepcopy(a))
-            check_memory_region(a, copy.deepcopy(a), b, f)
+                f = copy.deepcopy(b)
+                assert numpy.allclose(a, numpy.asarray(f))
+                check_flags(f, copy.deepcopy(a))
+                check_memory_region(a, copy.deepcopy(a), b, f)
 
-            g = copy.copy(b.view())
-            assert numpy.allclose(a, numpy.asarray(g))
-            check_memory_region(a, copy.copy(a.view()), b, g)
-            check_flags(g, copy.copy(a.view()))
+                g = copy.copy(b.view())
+                assert numpy.allclose(a, numpy.asarray(g))
+                check_memory_region(a, copy.copy(a.view()), b, g)
+                check_flags(g, copy.copy(a.view()))
 
 
 def test_len():
     for shp in [(5,),(6,7),(4,8,9),(1,8,9)]:
         for dtype in dtypes_all:
-            a = numpy.asarray(numpy.random.rand(*shp), dtype=dtype)
-            a_gpu = gpu_ndarray.GpuNdArrayObject(a)
-            assert len(a_gpu) == shp[0]
+            for offseted in [True, False]:
+                a, a_gpu = gen_gpu_nd_array(shp, dtype, offseted)
+                assert len(a_gpu) == shp[0]
 
 def test_mapping_getitem_w_int():
     def _cmp(x,y):
@@ -256,71 +278,68 @@ def test_mapping_getitem_w_int():
             raise Exception("Did not generate value error")
 
     for dtype in dtypes_all:
+        for offseted in [True, False]:
+            # test vector
+            dim = (2,)
+            a, _a = gen_gpu_nd_array(dim, dtype, offseted)
 
-        # test vector
-        dim =(2,)
-        a = numpy.asarray(numpy.random.rand(*dim)*10, dtype=dtype)
-        _a = gpu_ndarray.GpuNdArrayObject(a)
-        import sys
-        init_ref_count = sys.getrefcount(_a)
-        _cmp(_a[...], a[...])
-        _cmp(_a[...], a[...])
-        _cmp(_a[...], a[...])
-        _cmp(_a[...], a[...])
-        _cmp(_a[...], a[...])
+            import sys
+            init_ref_count = sys.getrefcount(_a)
+            _cmp(_a[...], a[...])
+            _cmp(_a[...], a[...])
+            _cmp(_a[...], a[...])
+            _cmp(_a[...], a[...])
+            _cmp(_a[...], a[...])
 
+            _cmp(_a[-1], a[-1])
+            _cmp(_a[1], a[1])
+            _cmp(_a[0], a[0])
+            _cmp(_a[::1], a[::1])
+            _cmpNs(_a[::-1], a[::-1])
+            _cmp(_a[...], a[...])
+            _cmpf(_a, 2)
 
-        _cmp(_a[-1], a[-1])
-        _cmp(_a[1], a[1])
-        _cmp(_a[0], a[0])
-        _cmp(_a[::1], a[::1])
-        _cmpNs(_a[::-1], a[::-1])
-        _cmp(_a[...], a[...])
-        _cmpf(_a,2)
+            # test scalar
+            dim = ()
+            a, _a = gen_gpu_nd_array(dim, dtype, offseted)
+            _cmp(_a[...], a[...])
+            _cmpf(_a, 0)
+            _cmpfV(_a, slice(1))
 
-        # test scalar
-        dim =()
-        a = numpy.asarray(numpy.random.rand(*dim)*10, dtype=dtype)
-        _a = gpu_ndarray.GpuNdArrayObject(a)
-        _cmp(_a[...], a[...])
-        _cmpf(_a,0)
-        _cmpfV(_a,slice(1))
+            # test 4d-tensor
+            dim = (5, 4, 3, 2)
+            a, _a = gen_gpu_nd_array(dim, dtype, offseted)
+            _cmpf(_a, slice(-1), slice(-1), 10, -10)
+            _cmpf(_a, slice(-1), slice(-1), -10, slice(-1))
+            _cmpf(_a, 0, slice(0, -1, -20), -10)
+            _cmpf(_a, 10)
+            _cmpf(_a, (10, 0, 0, 0))
+            _cmpf(_a, -10)
 
-        # test 4d-tensor
-        dim =(5,4,3,2)
-        a = numpy.asarray(numpy.random.rand(*dim)*10, dtype=dtype)
-        _a = gpu_ndarray.GpuNdArrayObject(a)
-        _cmpf(_a,slice(-1),slice(-1),10,-10)
-        _cmpf(_a,slice(-1),slice(-1),-10,slice(-1))
-        _cmpf(_a,0,slice(0,-1,-20),-10)
-        _cmpf(_a,10)
-        _cmpf(_a,(10,0,0,0))
-        _cmpf(_a,-10)
+            #test with integer
+            _cmp(_a[1], a[1])
+            _cmp(_a[-1], a[-1])
+            _cmp(_a[numpy.int64(1)], a[numpy.int64(1)])
+            _cmp(_a[numpy.int64(-1)], a[numpy.int64(-1)])
 
-        #test with integer
-        _cmp(_a[1], a[1])
-        _cmp(_a[-1], a[-1])
-        _cmp(_a[numpy.int64(1)], a[numpy.int64(1)])
-        _cmp(_a[numpy.int64(-1)], a[numpy.int64(-1)])
+            #test with slice
+            _cmp(_a[1:], a[1:])
+            _cmp(_a[1:2], a[1:2])
+            _cmp(_a[-1:1], a[-1:1])
 
-        #test with slice
-        _cmp(_a[1:], a[1:])
-        _cmp(_a[1:2], a[1:2])
-        _cmp(_a[-1:1], a[-1:1])
+            #test with tuple (mix slice, integer, numpy.int64)
+            _cmpNs(_a[0, 0, ::numpy.int64(-1), ::-1], a[0, 0, ::-1, ::-1])
+            _cmpNs(_a[:, :, ::numpy.int64(-1), ::-1], a[:, :, ::-1, ::-1])
+            _cmpNs(_a[:, :, numpy.int64(1), -1], a[:, :, 1, -1])
+            _cmpNs(_a[:, :, ::-1, ::-1], a[:, :, ::-1, ::-1])
+            _cmpNs(_a[:, :, ::-10, ::-10], a[:, :, ::-10, ::-10])
+            _cmpNs(_a[:, :, 1, -1], a[:, :, 1, -1])
+            _cmpNs(_a[:, :, -1, :], a[:, :, -1, :])
+            _cmpNs(_a[:, ::-2, -1, :], a[:, ::-2, -1, :])
+            _cmpNs(_a[:, ::-20, -1, :], a[:, ::-20, -1, :])
+            _cmpNs(_a[:, ::-2, -1], a[:, ::-2, -1])
+            _cmpNs(_a[0, ::-2, -1], a[0, ::-2, -1])
+            _cmp(_a[-1, -1, -1, -2], a[-1, -1, -1, -2])
 
-        #test with tuple (mix slice, integer, numpy.int64)
-        _cmpNs(_a[0,0,::numpy.int64(-1), ::-1], a[0,0,::-1,::-1])
-        _cmpNs(_a[:,:,::numpy.int64(-1), ::-1], a[:,:,::-1,::-1])
-        _cmpNs(_a[:,:,numpy.int64(1),-1], a[:,:,1,-1])
-        _cmpNs(_a[:,:,::-1, ::-1], a[:,:,::-1,::-1])
-        _cmpNs(_a[:,:,::-10, ::-10], a[:,:,::-10,::-10])
-        _cmpNs(_a[:,:,1,-1], a[:,:,1,-1])
-        _cmpNs(_a[:,:,-1,:], a[:,:,-1,:])
-        _cmpNs(_a[:,::-2,-1,:], a[:,::-2,-1,:])
-        _cmpNs(_a[:,::-20,-1,:], a[:,::-20,-1,:])
-        _cmpNs(_a[:,::-2,-1], a[:,::-2,-1])
-        _cmpNs(_a[0,::-2,-1], a[0,::-2,-1])
-        _cmp(_a[-1,-1,-1,-2], a[-1,-1,-1,-2])
-
-        #test ellipse
-        _cmp(_a[...], a[...])
+            #test ellipse
+            _cmp(_a[...], a[...])
