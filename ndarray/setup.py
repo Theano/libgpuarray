64de4ce97c8432a76b6396814bb1afce2265f393
Arnaud Bergeron <abergeron@gmail.com>
2012-02-13 14:20:14 -0500

    Make setup.py detect CUDA and OpenCL and include them conditionally.

--- ndarray/setup.py
+++ ndarray/setup.py
@@ -2,6 +2,8 @@ import os, sys
 import subprocess
 
 have_cython = False
+have_cuda = None
+have_opencl = None
 
 try:
     from Cython.Distutils import build_ext
@@ -10,81 +12,143 @@ except:
     from distutils.command.build_ext import build_ext
 
 from distutils.core import setup, Extension
-from distutils.dep_util import newer
 
 import numpy as np
 
-class build_ext_nvcc(build_ext):
-    user_options = build_ext.user_options
-    user_options.extend([
-            ('cuda-root=', None, "The cuda root directory")])
-    
-    def initialize_options(self):
-        build_ext.initialize_options(self)
-        self.cuda_root = None
-
-    def finalize_options(self):
-        build_ext.finalize_options(self)
-        if self.cuda_root is None:
-            self.cuda_root = os.getenv('CUDA_ROOT', None)
-        if self.cuda_root is not None:
-            self._nvcc_bin = os.path.join(self.cuda_root, 'bin', 'nvcc')
-        else:
-            self._nvcc_bin = 'nvcc'
-
-    def cuda_process(self, source, include_args):
-        target = source+'.cpp'
-        if newer(source, target):
-            self.spawn([self._nvcc_bin, '--cuda', source, '-o', target] + \
-                           include_args)
-        return target
-
-    def cuda_extension(self, ext):
-        includes = self.distribution.include_dirs+ext.include_dirs
-        include_args = ['-I'+i for i in includes]
-        new_sources = []
-        anycuda = False
-        for src in ext.sources:
-            if src.endswith('.cu'):
-                new_sources.append(self.cuda_process(src, include_args))
-                anycuda = True
-            else:
-                new_sources.append(src)
-        if anycuda:
-            ext.sources = new_sources
-            if self.cuda_root is not None:
-                lib = os.path.join(self.cuda_root, 'lib')
-                lib64 = os.path.join(self.cuda_root, 'lib64')
-                if os.path.isdir(lib):
-                    ext.library_dirs.append(lib)
-                    ext.extra_link_args.append('-Xlinker')
-                    ext.extra_link_args.append('-rpath')
-                    ext.extra_link_args.append('-Xlinker')                    
-                    ext.extra_link_args.append(lib)
-                if os.path.isdir(lib64):
-                    ext.library_dirs.append(lib64)
-                    ext.extra_link_args.append('-Xlinker')
-                    ext.extra_link_args.append('-rpath')
-                    ext.extra_link_args.append('-Xlinker')
-                    ext.extra_link_args.append(lib64)
-            if 'cudart' not in ext.libraries:
-                ext.libraries.append('cudart')
-    
-    def build_extensions(self):
-        self.check_extensions_list(self.extensions)
-
-        for ext in self.extensions:
-            self.cuda_extension(ext)
-            if have_cython:
-                ext.sources = self.cython_sources(ext.sources, ext)
-            self.build_extension(ext)
-
 # These are always there
 srcs = ['compyte_types.c', 'compyte_util.c', 'compyte_buffer.c']
+macros = []
+include_dirs = [np.get_include(), '.']
+lib_dirs = []
+libraries = []
+ext_link_args = []
 
-# Will need some way of detecting those
-srcs.append('compyte_buffer_cuda.cu')
-srcs.append('compyte_buffer_opencl.c')
+fnull = open(os.devnull, 'r+')
+
+# Detect CUDA install
+def try_cuda(arg):
+    global have_cuda
+    print "Searching for CUDA..."
+    if arg is None:
+        cuda_root = os.getenv('CUDA_ROOT')
+    else:
+        cuda_root = arg
+    if cuda_root is None:
+        cuda_bin_prefix = ''
+    else:
+        cuda_bin_prefix = os.path.join(cuda_root, 'bin')+os.path.sep
+    
+    try:
+        subprocess.check_call([os.path.join(cuda_bin_prefix, 'nvcc'),
+                               '--version'], stdout=fnull, stderr=fnull)
+    except Exception:
+        have_cuda = False
+        return
+
+    print "Found nvcc at:", os.path.join(cuda_bin_prefix, 'nvcc')
+    macros.append(('WITH_CUDA', '1'))
+    macros.append(('CUDA_BIN_PATH', '"'+cuda_bin_prefix+'"'))
+    srcs.append('compyte_buffer_cuda.c')
+    
+    if sys.platform == 'darwin':
+        print "Mac platform, using CUDA framework"
+        ext_link_args.append('-framework')
+        ext_link_args.append('CUDA')
+    else:
+        libraries.append('cuda')
+        if cuda_root is None:
+            print "WARNING: no CUDA_ROOT specified, assuming it is part of the default search path"
+        else:
+            include_dirs.append(os.path.join(cuda_root, 'include'))
+            lib = os.path.join(self.cuda_root, 'lib')
+            lib64 = os.path.join(self.cuda_root, 'lib64')
+            if os.path.isdir(lib):
+                lib_dirs.append(lib)
+                ext_link_args.append('-Xlinker')
+                ext_link_args.append('-rpath')
+                ext_link_args.append('-Xlinker')
+                ext_link_args.append(lib)
+            if os.path.isdir(lib64):
+                lib_dirs.append(lib64)
+                ext_link_args.append('-Xlinker')
+                ext_link_args.append('-rpath')
+                ext_link_args.append('-Xlinker')
+                ext_link_args.append(lib64)
+    have_cuda = True
+
+def enable_cuda(arg):
+    try_cuda(arg)
+    if not have_cuda:
+        print "Could not find CUDA",
+        if arg is not None:
+            print "at the specified location (%s)"%(arg,)
+        else:
+            print "try specifying the cuda root (either as argument to --enable-cuda or by the envionnement variable CUDA_ROOT)"
+        raise Exception("Could not find CUDA")
+
+def try_opencl(arg):
+    global have_opencl
+    print "Searching for OpenCL..."
+    if sys.platform == 'darwin':
+        if int(os.uname()[2].split('.')[0]) <= 9:
+            have_opencl = False
+            return
+        print "On Mac >= 10.6, using framework"
+        ext_link_args.append('-framework')
+        ext_link_args.append('OpenCL')
+    else:
+        print "Assuming OpenCL install in default search paths"
+        libraries.append('OpenCL')
+    srcs.append('compyte_buffer_opencl.c')
+    macros.append(('WITH_OPENCL', '1'))
+    have_opencl = True
+
+def enable_opencl(arg):
+    try_opencl(arg)
+    if not have_opencl:
+        print "Could not find OpenCL"
+        if sys.platform == 'darwin':
+            print "OpenCL support on Mac starts with 10.6, please upgrade"
+        else:
+            print "Install an OpenCL runtime."
+
+to_del = []
+
+for i, a in enumerate(sys.argv):
+    if a.startswith('--enable-cuda'):
+        to_del.append(i)
+        s = a.split('=', 1)
+        if len(s) == 1:
+            enable_cuda(None)
+        else:
+            enable_cuda(s[1])
+    elif a.startswith('--enable-opencl'):
+        to_del.append(i)
+        s = a.split('=', 1)
+        if len(s) == 1:
+            enable_opencl(None)
+        else:
+            enable_opencl(s[1])
+    elif a == '--disable-cuda':
+        to_del.append(i)
+        have_cuda = False
+    elif a == '--disable-opencl':
+        to_del.append(i)
+        have_opencl = False
+    elif a == 'disable-cython':
+        to_del.append(i)
+        have_cython = False
+
+for i in reversed(to_del):
+    del sys.argv[i]
+
+del to_del
+
+if have_cuda is None:
+    try_cuda(None)
+
+if have_opencl is None:
+    try_opencl(None)
 
 if have_cython:
     srcs.append('pygpu_ndarray.pyx')
@@ -92,11 +156,13 @@ else:
     srcs.append('pygpu_ndarray.c')
 
 setup(name='compyte',
-      cmdclass = {'build_ext': build_ext_nvcc},
-      include_dirs = [np.get_include(), '.'],
-      ext_modules=[Extension('compyte_buffer',
-                             define_macros = [('WITH_CUDA','1'), ('WITH_OPENCL','1')],
-                             sources = srcs
+      cmdclass = {'build_ext': build_ext},
+      ext_modules=[Extension('pygpu_ndarray',
+                             define_macros = macros,
+                             sources = srcs,
+                             include_dirs = include_dirs,
+                             libraries = libraries,
+                             library_dirs = lib_dirs,
+                             extra_link_args = ext_link_args,
                              )
-                   ]
-)
+                   ])
