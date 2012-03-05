0646fc68dd4bf53df13a54584bf72cf3b9d9c329
Arnaud Bergeron <abergeron@gmail.com>
2012-03-05 12:21:24 -0500

    Handle the error message a bit better.  (This is a special situation, so it's a bit ugly).

--- ndarray/pygpu_ndarray.pyx
+++ ndarray/pygpu_ndarray.pyx
@@ -234,13 +234,19 @@ def init(kind, int devno):
     if kind == "opencl":
         ctx = opencl_ops.buffer_init(devno, &err)
         if (err != GA_NO_ERROR):
-            raise GpuArrayException(opencl_ops.buffer_error())
+            if err == GA_VALUE_ERROR:
+                raise GpuArrayException("No device %d"%(devno,))
+            else:
+                raise GpuArrayException(opencl_ops.buffer_error())
     cendif()
     cifcuda()
     if kind == "cuda":
         ctx = cuda_ops.buffer_init(devno, &err)
         if (err != GA_NO_ERROR):
-            raise GpuArrayException(cuda_ops.buffer_error())
+            if err == GA_VALUE_ERROR:
+                raise GpuArrayException("No device %d"%(devno,))
+            else:
+                raise GpuArrayException(cuda_ops.buffer_error())
     cendif()
     if ctx == NULL:
         raise RuntimeError("Unsupported kind: %s"%(kind,))
