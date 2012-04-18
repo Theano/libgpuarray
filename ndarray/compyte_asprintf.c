2201e6bc78c2fc105ceac8cb91a86813ba719300
Arnaud Bergeron <abergeron@gmail.com>
2012-04-17 21:49:31 -0400

    Make compyte_asprintf compilable and fix indentation.

--- ndarray/compyte_asprintf.c
+++ ndarray/compyte_asprintf.c
@@ -1,23 +1,28 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include "compyte_compat.h"
 
 #include <stdarg.h>
+#include <stdlib.h>
 
 int asprintf(char **ret, const char *fmt, ...) {
-    va_list ap;
-    char buf[1]
-    char *res;
-    int size;
+  va_list ap;
+  char buf[1];
+  char *res;
+  int size;
 
-    va_start(ap, fmt);
-    size = vsnprintf(buf, sizeof(buf), fmt, ap);
-    va_end(ap);
+  va_start(ap, fmt);
+  size = vsnprintf(buf, sizeof(buf), fmt, ap);
+  va_end(ap);
 
-    res = malloc(size+1);
-    if (res == NULL) return -1;
+  res = malloc(size+1);
+  if (res == NULL) return -1;
 
-    va_start(ap, fmt);
-    size = vsnprintf(res, size+1, fmt, ap);
-    va_end(ap);
-    *ret = res;
-    return size;
+  va_start(ap, fmt);
+  size = vsnprintf(res, size+1, fmt, ap);
+  va_end(ap);
+  *ret = res;
+  return size;
 }
