c5c653814e71164c69b7ebafa1f67737c72d79b6
Arnaud Bergeron <abergeron@gmail.com>
2012-08-10 13:17:58 -0400

    Elemwise noew has all the base classes to run kernels.

--- elemwise.py
+++ elemwise.py
@@ -3,6 +3,10 @@ from mako.template import Template
 from tools import ScalarArg, ArrayArg
 from dtypes import parse_c_arg_backend
 
+import numpy
+from ndarray import pygpu_ndarray as gpuarray
+
+
 # parameters: preamble, name, nd, arguments, expression
 basic_kernel = Template("""
 ${preamble}
@@ -72,54 +76,6 @@ KERNEL void ${name}(const unsigned int n
 }
 """)
 
-
-# arguments: preamble, name, arguments, expression
-generic_kernel = Template("""
-${preamble}
-
-KERNEL void ${name}(const unsigned int n,
-                    const unsigned int nd,
-                    const GLOBAL_MEM unsigned int *dims,
-                    const GLOBAL_MEM int *strs
-% for arg in arguments:
-                    , ${arg.decltype()} ${arg.name}_data
-% endfor
-) {
-  const unsigned int idx = LDIM_0 * GID_0 + LID_0;
-  const unsigned int numThreads = LDIM_0 * GDIM_0;
-  unsigned int i;
-
-  for (i = idx; i < n; i += numThreads) {
-    unsigned int ii = i;
-    unsigned int pos;
-% for arg in arguments:
-    % if arg.isarray():
-    GLOBAL_MEM char *${arg.name}_p = (GLOBAL_MEM char *)${arg.name}_data;
-    % endif
-% endfor
-    for (unsigned int d = nd-1; d > 0; d--) {
-        pos = ii % dims[d];
-        ii = ii / dims[d];
-    % for a, arg in enumerate(arguments):
-        % if arg.isarray():
-        ${arg.name}_p += pos * strs[(nd*${a})+d];
-        % endif
-    % endfor
-    }
-    pos = ii;
-    % for a, arg in enumerate(arguments):
-        % if arg.isarray():
-    ${arg.name}_p += pos * strs[nd*${a}];
-        % endif
-    % endfor
-    % for arg in arguments:
-    ${arg.decltype()} ${arg.name} = (${arg.decltype()})${arg.name}_p;
-    % endfor
-    ${expression};
-  }
-}
-""")
-
 # arguments: preamble, name, arguments, n, nd, dim, strs, expression
 specialized_kernel = Template("""
 ${preamble}
@@ -176,13 +132,19 @@ INDEX_RE = re.compile('([a-zA-Z_][a-zA-Z0-9_]*)\[i\]')
 def massage_op(operation):
     return INDEX_RE.sub('\g<1>[0]', operation)
 
+class ElemwiseKernelBase(object):
+    def __init__(self, kind, context, arguments, operation, preamble=""):
+        self._cache = dict()
 
-class ElemwiseKernel(object):
-    def __init__(self, kind, context, arguments, operation, name="kernel", preamble="", options=[]):
         if isinstance(arguments, str):
             self.arguments = parse_c_args(arguments)
         else:
             self.arguments = arguments
+
+        self.operation = operation
+        self.expression = massage_op(operation)
+        self.kind = kind
+        self.context = context
 
         if not any(isinstance(arg, ArrayArg) for arg in self.arguments):
             raise RuntimeError(
@@ -194,6 +156,8 @@ class ElemwiseKernel(object):
         have_bytestore_pragma = False
         have_double_pragma = False
         for arg in self.arguments:
+            # Revise this so that we just pass a parameter and let the
+            # backend fuss over the details
             if arg.dtype.itemsize < 4 and type(arg) == ArrayArg \
                     and kind == 'opencl':
                 # XXX: On ATI cards we must check that the device
@@ -212,179 +176,113 @@ class ElemwiseKernel(object):
                             "#pragma OPENCL EXTENSION cl_khr_fp64: enable")
                     have_double_pragma = True
 
-            if extra_preamble:
-                preamble = '\n'.join(extra_preamble) + '\n' + preamble
+        self.preamble = '\n'.join(extra_preamble) + '\n' + preamble + '\n' \
+            + gpuarray.get_preamble(kind)
+        self.setup()
 
+    def setup(self):
+        pass
 
-import numpy
-from ndarray import pygpu_ndarray as gpuarray
-kind = "opencl"
-ctx = gpuarray.init(kind, 0)
-preamble = gpuarray.get_preamble(kind)
+    def compile(self, args):
+        raise NotImplementedError()
 
-def call_elemwise_gen(kind, ctx, args, c_args, operation):
-    arguments = parse_c_args(c_args)
-    expression = massage_op(operation)
-    source = generic_kernel.render(preamble=preamble, name="elemk",
-                                   arguments=arguments,
-                                   expression=expression)
-    k = gpuarray.GpuKernel(source, "elemk", kind=kind, context=ctx)
-    kernel_args = []
-    n = 0
-    nd = 0
-    dims = None
-    strs = []
-    for arg in args:
-        if isinstance(arg, gpuarray.GpuArray):
-            n = arg.size
-            nd = arg.ndim
-            dims = arg.shape
-            kernel_args.append(arg)
-            strs.extend(arg.strides)
-#            kernel_args.append(gpuarray.array(arg.strides, dtype='int32',
-#                                              order='C',
-#                                              kind=kind, context=ctx))
-        else:
-            kernel_args.append(arg)
+    def __call__(self, *args):
+        k = self.compile(args)
+        k(*self.kernel_args, n=self.n)
 
-    if dims is None:
-        raise ValueError("No array arguments")
-    if n == 0:
-        return # no work to be done.
+class ElemwiseKernelBasic(ElemwiseKernelBase):
+    def compile(self, args):
+        nd = self.prepare_args(args)
+        if nd not in self._cache:
+            src = basic_kernel.render(preamble=self.preamble, name="elemk",
+                                      nd=nd, arguments=self.arguments,
+                                      expression=self.expression)
+            self._cache[nd] = gpuarray.GpuKernel(src, "elemk",
+                                                 kind=self.kind,
+                                                 context=self.context)
+        return self._cache[nd]
 
-    kernel_args.insert(0, gpuarray.array(strs, dtype='int32', order='C',
-                                         kind=kind, context=ctx))
-    kernel_args.insert(0, gpuarray.array(dims, dtype='uint32', order='C',
-                                         kind=kind, context=ctx))
-    kernel_args.insert(0, numpy.asarray(nd, 'uint32'))
-    kernel_args.insert(0, numpy.asarray(n, 'uint32'))
-    k(*kernel_args, n=n)
+    def prepare_args(self, args):
+        n = 0
+        nd = 0
+        dims = None
+        kernel_args = []
+        for arg in args:
+            if isinstance(arg, gpuarray.GpuArray):
+                n = arg.size
+                nd = arg.ndim
+                dims = arg.shape
+                kernel_args.append(arg),
+                kernel_args.extend(numpy.asarray(s, dtype='int32') \
+                                       for s in arg.strides)
+            else:
+                kernel_args.append(arg)
 
-def call_elemwise_basic(kind, ctx, args, c_args, operation):
-    arguments = parse_c_args(c_args)
-    expression = massage_op(operation)
-    n = 0
-    nd = 0
-    dims = None
-    kernel_args = []
-    for arg in args:
-        if isinstance(arg, gpuarray.GpuArray):
-            n = arg.size
-            nd = arg.ndim
-            dims = arg.shape
-            kernel_args.append(arg),
-            kernel_args.extend(numpy.asarray(s, dtype='int32') \
-                                   for s in arg.strides)
-        else:
-            kernel_args.append(arg)
+        for d in reversed(dims):
+            kernel_args.insert(0, numpy.asarray(d, dtype='uint32'))
 
-    for d in reversed(dims):
-        kernel_args.insert(0, numpy.asarray(d, dtype='uint32'))
-    kernel_args.insert(0, numpy.asarray(n, dtype='uint32'))
-    source = basic_kernel.render(preamble=preamble, name="elemk",
-                                 nd=nd, arguments=arguments,
-                                 expression=expression)
-    k = gpuarray.GpuKernel(source, "elemk", kind=kind, context=ctx)
-    k(*kernel_args, n=n)
+        kernel_args.insert(0, numpy.asarray(n, dtype='uint32'))
 
+        self.n = n
+        self.kernel_args = kernel_args
+        return nd
 
-def call_elemwise_specialized(kind, ctx, args, c_args, operation):
-    arguments = parse_c_args(c_args)
-    expression = massage_op(operation)
-    n = 0
-    nd = 0
-    dims = None
-    kernel_args = []
-    strs = []
-    for arg in args:
-        if isinstance(arg, gpuarray.GpuArray):
-            n = arg.size
-            nd = arg.ndim
-            dims = arg.shape
-            kernel_args.append(arg)
-            strs.append(arg.strides)
-        else:
-            kernel_args.append(arg)
-            strs.append(None)
+class ElemwiseKernelSpecialized(ElemwiseKernelBase):
+    def compile(self, args):
+        nd, dims, strs = self.prepare_args(args)
+        key = nd, dims, tuple(strs)
+        if key not in self._cache:
+            src = specialized_kernel.render(preamble=self.preamble,
+                                            name="elemk", n=self.n, nd=nd,
+                                            dim=dims, strs=strs,
+                                            arguments=self.arguments,
+                                            expression=self.expression)
+            self._cache[key] = gpuarray.GpuKernel(src, "elemk",
+                                                  kind=self.kind,
+                                                  context=self.context)
+        return self._cache[key]
 
-    source = specialized_kernel.render(preamble=preamble, name="elemk",
-                                       n=n, nd=nd, dim=dims, strs=strs,
-                                       arguments=arguments,
-                                       expression=expression)
-    k = gpuarray.GpuKernel(source, "elemk", kind=kind, context=ctx)
-    k(*kernel_args, n=n)
+    def prepare_args(self, args):
+        n = 0
+        nd = 0
+        dims = None
+        kernel_args = []
+        strs = []
+        for arg in args:
+            if isinstance(arg, gpuarray.GpuArray):
+                n = arg.size
+                nd = arg.ndim
+                dims = arg.shape
+                kernel_args.append(arg)
+                strs.append(arg.strides)
+            else:
+                kernel_args.append(arg)
+                strs.append(None)
+        self.n = n
+        self.kernel_args = kernel_args
+        return nd, dims, strs
 
-def call_elemwise_contig(kind, ctx, args, c_args, operation):
-    arguments = parse_c_args(c_args)
-    source = contiguous_kernel.render(preamble=preamble, name="elemk",
-                                      arguments=arguments,
-                                      expression=operation)
-    n = None
-    kernel_args = []
-    for arg in args:
-        if isinstance(arg, gpuarray.GpuArray):
-            n = arg.size
-            kernel_args.append(arg),
-        else:
-            kernel_args.append(arg)
-    kernel_args.insert(0, numpy.asarray(n, 'uint32'))
-    k = gpuarray.GpuKernel(source, "elemk", kind=kind, context=ctx)
-    k(*kernel_args, n=n)
+class ElemwiseKernelContig(ElemwiseKernelBase):
+    def setup(self):
+        src = contiguous_kernel.render(preamble=self.preamble, name="elemk",
+                                       arguments=self.arguments,
+                                       expression=self.operation)
+        self.k = gpuarray.GpuKernel(src, "elemk", kind=self.kind,
+                                    context=self.context)
 
-a = numpy.random.random((500, 1000)).astype('float32')
-b = numpy.random.random((500, 1000)).astype('float32')
-c = a + b
-gpua = gpuarray.array(a, kind=kind, context=ctx)
-gpub = gpuarray.array(b, kind=kind, context=ctx)
-gpuc = gpuarray.empty(c.shape, dtype=c.dtype, kind=kind, context=ctx)
-def smoketest(f):
-    f(kind, ctx, (gpua, gpub, gpuc), "float *a, float *b, float *c", "c[i] = a[i] + b[i]")
+    def compile(self, args):
+        n = None
+        kernel_args = []
+        for arg in args:
+            if isinstance(arg, gpuarray.GpuArray):
+                n = arg.size
+                kernel_args.append(arg),
+            else:
+                kernel_args.append(arg)
 
-smoketest(call_elemwise_gen)
-assert (c == numpy.asarray(gpuc)).all()
-gpuc = gpuarray.empty(c.shape, dtype=c.dtype, kind=kind, context=ctx)
-smoketest(call_elemwise_basic)
-assert (c == numpy.asarray(gpuc)).all()
-gpuc = gpuarray.empty(c.shape, dtype=c.dtype, kind=kind, context=ctx)
-smoketest(call_elemwise_specialized)
-assert (c == numpy.asarray(gpuc)).all()
-gpuc = gpuarray.empty(c.shape, dtype=c.dtype, kind=kind, context=ctx)
-smoketest(call_elemwise_contig)
-assert (c == numpy.asarray(gpuc)).all()
+        kernel_args.insert(0, numpy.asarray(n, dtype='uint32'))
 
-import gc, time, math
+        self.kernel_args = kernel_args
+        self.n = n
 
-def timeit(f, lbl):
-
-    gc.disable()
-    t = time.time()
-    f()
-    est = time.time() - t
-    gc.enable()
-
-    loops = max(1, int(10**math.floor(math.log(10/est, 10))))
-
-    gc.disable()
-    t = time.time()
-    for _ in xrange(loops):
-        f()
-
-    print lbl, "(", loops, "loops ):", (time.time() - t)/loops, "s"
-    gc.enable()
-
-def f_basic():
-    smoketest(call_elemwise_basic)
-
-def f_gen():
-    smoketest(call_elemwise_gen)
-
-def f_spec():
-    smoketest(call_elemwise_specialized)
-
-def f_contig():
-    smoketest(call_elemwise_contig)
-
-timeit(f_basic, 'basic')
-timeit(f_gen, 'generic')
-timeit(f_spec, 'specialized')
-timeit(f_contig, 'contiguous')
+        return self.k
