96237909b0aa29397cecd27661a580b48512eb8e
Arnaud Bergeron <abergeron@gmail.com>
2012-08-23 18:41:01 -0400

    Reorganize tests and add some tests for inplace operations.

--- tests/test_elemwise.py
+++ tests/test_elemwise.py
@@ -8,17 +8,29 @@ from .support import (guard_devsup, rand, check_flags, check_meta, check_all,
                       kind, context, gen_gpuarray, dtypes_no_complex)
 
 
-def test_elemwise_ops_array():
-    for op in [operator.add, operator.floordiv, operator.mod,
-               operator.mul, operator.truediv]:
+operators2 = [operator.add, operator.sub, operator.div, operator.floordiv,
+              operator.mod, operator.mul, operator.truediv]
+ioperators2 = [operator.iadd]
+
+
+def test_elemwise2_ops_array():
+    for op in operators2:
         for dtype1 in dtypes_no_complex:
             for dtype2 in dtypes_no_complex:
                 for shape in [(500,), (50, 5), (5, 6, 7)]:
-                    yield elemwise_ops_array, op, dtype1, dtype2, shape
+                    yield elemwise2_ops_array, op, dtype1, dtype2, shape
+
+
+def test_ielemwise2_ops_array():
+    for op in ioperators2:
+        for dtype1 in dtypes_no_complex:
+            for dtype2 in dtypes_no_complex:
+                for shape in [(500,), (50, 5), (5, 6, 7)]:
+                    yield ielemwise2_ops_array, op, dtype1, dtype2, shape
 
 
 @guard_devsup
-def elemwise_ops_array(op, dtype1, dtype2, shape):
+def elemwise2_ops_array(op, dtype1, dtype2, shape):
     ac, ag = gen_gpuarray(shape, dtype1, kind=kind, ctx=context)
     bc, bg = gen_gpuarray(shape, dtype2, nozeros=True, kind=kind, ctx=context)
 
@@ -30,26 +42,48 @@ def elemwise_ops_array(op, dtype1, dtype2, shape):
     assert numpy.allclose(out_c, numpy.asarray(out_g))
 
 
-def test_elemwise_ops_mixed():
-    for op in [operator.add, operator.floordiv, operator.mod,
-               operator.mul, operator.truediv]:
+@guard_devsup
+def ielemwise2_ops_array(op, dtype1, dtype2, shape):
+    ac, ag = gen_gpuarray(shape, dtype1, kind=kind, ctx=context)
+    bc, bg = gen_gpuarray(shape, dtype2, nozeros=True, kind=kind, ctx=context)
+
+    out_c = op(ac, bc)
+    out_g = op(ag, bg)
+
+    assert out_g is ag
+    assert out_c.shape == out_g.shape
+    assert out_c.dtype == out_g.dtype
+    assert numpy.allclose(out_c, numpy.asarray(out_g))
+
+
+def test_elemwise2_ops_mixed():
+    for op in operators2:
         for dtype in dtypes_no_complex:
             for shape in [(500,), (50, 5), (5, 6, 7)]:
                 for elem in [2, 0.3, numpy.asarray(3, dtype='uint8'),
                              numpy.asarray(7, dtype='uint32'),
                              numpy.asarray(2.45, dtype='float32')]:
-                    yield elemwise_ops_mixed, op, dtype, shape, elem
+                    yield elemwise2_ops_mixed, op, dtype, shape, elem
+
+
+def test_ielemwise2_ops_mixed():
+    for op in ioperators2:
+        for dtype in dtypes_no_complex:
+            for shape in [(500,), (50, 5), (5, 6, 7)]:
+                for elem in [2, 0.3, numpy.asarray(3, dtype='uint8'),
+                             numpy.asarray(7, dtype='uint32'),
+                             numpy.asarray(2.45, dtype='float32')]:
+                    yield ielemwise2_ops_mixed, op, dtype, shape, elem
 
 
 @guard_devsup
-def elemwise_ops_mixed(op, dtype, shape, elem):
+def elemwise2_ops_mixed(op, dtype, shape, elem):
     c, g = gen_gpuarray(shape, dtype, nozeros=True, kind=kind, ctx=context)
 
     out_c = op(c, elem)
     out_g = op(g, elem)
 
     assert out_c.shape == out_g.shape
-    print out_c.dtype, out_g.dtype
     assert out_c.dtype == out_g.dtype
     assert numpy.allclose(out_c, numpy.asarray(out_g))
 
@@ -57,7 +91,19 @@ def elemwise_ops_mixed(op, dtype, shape, elem):
     out_g = op(elem, g)
 
     assert out_c.shape == out_g.shape
-    print out_c.dtype, out_g.dtype
+    assert out_c.dtype == out_g.dtype
+    assert numpy.allclose(out_c, numpy.asarray(out_g))
+
+
+@guard_devsup
+def ielemwise2_ops_mixed(op, dtype, shape, elem):
+    c, g = gen_gpuarray(shape, dtype, kind=kind, ctx=context)
+
+    out_c = op(c, elem)
+    out_g = op(g, elem)
+
+    assert out_g is g
+    assert out_c.shape == out_g.shape
     assert out_c.dtype == out_g.dtype
     assert numpy.allclose(out_c, numpy.asarray(out_g))
 
@@ -87,8 +133,6 @@ def divmod_array(dtype1, dtype2, shape):
     assert out_c[1].shape == out_g[1].shape
     assert out_c[0].dtype == out_g[0].dtype
     assert out_c[1].dtype == out_g[1].dtype
-    if not numpy.allclose(out_c[0], numpy.asarray(out_g[0])):
-        import pdb; pdb.set_trace()
     assert numpy.allclose(out_c[0], numpy.asarray(out_g[0]))
     assert numpy.allclose(out_c[1], numpy.asarray(out_g[1]))
 
