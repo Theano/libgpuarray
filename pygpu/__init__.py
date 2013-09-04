92d0128ba34a04bc11d985764796349f386ee963
Arnaud Bergeron <abergeron@gmail.com>
2013-09-04 12:22:22 -0400

    Change the include style to <gpuarray_api.h>.

--- pygpu/__init__.py
+++ pygpu/__init__.py
@@ -1,7 +1,7 @@
 def get_include():
     import os.path
-    p = os.path.dirname(os.path.dirname(__file__))
-    assert os.path.exists(os.path.join(p, 'pygpu/gpuarray.h'))
+    p = os.path.dirname(__file__)
+    assert os.path.exists(os.path.join(p, 'gpuarray_api.h'))
     return p
 
 from . import gpuarray
