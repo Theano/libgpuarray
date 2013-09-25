bb11e49f706988a9a22a0be83bce893f75a58873
Arnaud Bergeron <abergeron@gmail.com>
2013-09-24 22:32:28 -0400

    Make the message associated with GA_VALUE_ERROR reflect the actual usage.

--- src/compyte_error.c
+++ src/compyte_error.c
@@ -8,7 +8,7 @@ const char *compyte_error_str(int err) {
   switch (err) {
   case GA_NO_ERROR:          return "No error";
   case GA_MEMORY_ERROR:      return "Out of memory";
-  case GA_VALUE_ERROR:       return "Value out of range";
+  case GA_VALUE_ERROR:       return "Value invalid or out of range";
   case GA_IMPL_ERROR:        return "Unknown device error";
   case GA_INVALID_ERROR:     return "Invalid value or operation";
   case GA_UNSUPPORTED_ERROR: return "Unsupported operation";
