921ba767bdbe3776da9844589756137e0552efe0
Arnaud Bergeron <abergeron@gmail.com>
2015-05-14 11:50:55 -0400

    Rework of the call protocol for kernel to lower overhead and allow more possibilities.
    You can now allocate dynamic shared memory and use any number of
    dimensions in the call (subject to backend limits).
    
    Also, this removes the scheduling feature from GpuKernel_call() itself
    and provides it as GpuKernel_sched() that you can call if you need it.

--- src/gpuarray/kernel.h
+++ src/gpuarray/kernel.h
@@ -77,30 +77,37 @@ GPUARRAY_PUBLIC void GpuKernel_clear(GpuKernel *k);
 GPUARRAY_PUBLIC void *GpuKernel_context(GpuKernel *k);
 
 /**
+ * Do a scheduling of local and global size for a kernel.
+ *
+ * This function will find an optimal grid and block size for the
+ * number of elements specified in n when running kernel k.  The
+ * parameters may run a bit more instances than n for efficiency
+ * reasons, so your kernel must be ready to deal with that.
+ *
+ * If either gs or ls is not 0 on entry its value will not be altered
+ * and will be taken into account when choosing the other value.
+ *
+ * \param k the kernel to schedule for
+ * \param n number of elements to handle
+ * \param ls local size (in/out)
+ * \param gs grid size (in/out)
+ */
+GPUARRAY_PUBLIC int GpuKernel_sched(GpuKernel *k, size_t n,
+                                    size_t *ls, size_t *gs);
+
+/**
  * Launch the execution of a kernel.
  *
- * You either specify the block and grid sizes (`ls` and `gs`) or the
- * total size (`n`). Set a value to `0` to indicate it is
- * unspecified. You can also specify the total size (`n`) and one of
- * the block (`ls`) or grid (`gs`) size.
- *
- * If you leave one or both of `ls` or `gs`, it will be filled
- * according to a heuristic to get a good performance out of your
- * hardware. However the number of kernel instances that will be run
- * can be slightly higher than the total size you specified in order
- * to avoid performance degradation. Your kernel should be ready to
- * handle this.
- *
  * \param k the kernel to launch
- * \param n number of instances to launch
- * \param ls size of launch blocks
- * \param gs size of launch grid
+ * \param n dimensionality of the grid/blocks
+ * \param ls sizes of launch blocks
+ * \param gs sizes of launch grid
+ * \param amount of dynamic shared memory to allocate
+ * \param args table of pointers to arguments
  */
-GPUARRAY_PUBLIC int GpuKernel_call2(GpuKernel *k, size_t n[2],
-                                   size_t ls[2], size_t gs[2], void **args);
-
-GPUARRAY_PUBLIC int GpuKernel_call(GpuKernel *k, size_t n,
-                                  size_t ls, size_t gs, void **args);
+GPUARRAY_PUBLIC int GpuKernel_call(GpuKernel *k, unsigned int n,
+                                   const size_t *ls, const size_t *gs,
+                                   size_t shared, void **args);
 
 GPUARRAY_PUBLIC int GpuKernel_binary(const GpuKernel *k, size_t *sz,
                                     void **obj);
