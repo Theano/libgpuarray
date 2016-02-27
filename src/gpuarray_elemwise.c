99b5675683bdb9e4f321f3ffd31e7d14999ae6ae
Arnaud Bergeron <abergeron@gmail.com>
2016-02-27 13:51:52 -0500

    WIP contig kernel generator + interface for calls.

--- src/gpuarray_elemwise.c
+++ src/gpuarray_elemwise.c
@@ -9,6 +9,20 @@ static void decl(strb *sb, int typecode, int isarray) {
     strb_appends(sb, ctype);
 }
 
+static void decl2(strb *sb, int typecode, void *arg) {
+  int isarray = 0;
+  if (typecode == GA_BUFFER) {
+    typecode = ((GpuArray *)arg)->typecode;
+    isarray = 1;
+  }
+  const char *ctype = gpuarray_get_type(typecode).cluda_name;
+  if (ctype == NULL) strb_seterror(sb);
+  if (isarray)
+    strb_appendf(sb, "GLOBAL_MEM %s*", ctype);
+  else
+    strb_appends(sb, ctype);
+}
+
 static int gen_elemwise_basic_kernel(GpuKernel *k,
                                      const gpuarray_buffer_ops *ops,
                                      void *ctx, char **err_str,
@@ -35,8 +49,9 @@ static int gen_elemwise_basic_kernel(GpuKernel *k,
 
   apos = 0;
 
-  strb_appends(&sb, preamble);
-  strb_appends(&sb, "KERNEL void elem(const unsigned int n, ");
+  if (preamble)
+    strb_appends(&sb, preamble);
+  strb_appends(&sb, "\nKERNEL void elem(const unsigned int n, ");
   atypes[apos++] = GA_INT;
   for (i = 0; i < nd; i++) {
     strb_appendf(&sb "const ga_size dim%u", i);
@@ -102,7 +117,7 @@ static int gen_elemwise_basic_kernel(GpuKernel *k,
     }
   }
   strb_appends(&sb, expr);
-  strb_appends(&sb, "\n}\n}\n");
+  strb_appends(&sb, ";\n}\n}\n");
   if (strb_error(&sb)) {
     res = GA_MEMORY_ERROR;
     goto bail;
@@ -114,4 +129,77 @@ static int gen_elemwise_basic_kernel(GpuKernel *k,
   free(atypes);
   strb_clear(&sb);
   return res;
+}
+
+static int gen_elemwise_contig_kernel(GpuKernel *k,
+                                      const gpuarray_buffer_ops *ops,
+                                      void *ctx, char **err_str,
+                                      const char *preamble,
+                                      const char *expr,
+                                      gpuarray_args *args) {
+  strb sb = STRB_STATIC_INIT;
+  unsigned int j;
+  int flags = GA_USE_CLUDA;
+  int res;
+
+  for (j = 0; j < n; j++) {
+    flags |= gpuarray_type_flags(types[j], -1);
+  }
+
+  if (preamble)
+    strb_appends(&sb, preamble);
+  strb_appends(&sb, "\nKERNEL void elem(const ga_size n, ");
+  for (j = 0; j < args->n; j++) {
+    decl2(sb, args->types[j], args->args[j]);
+    if (args->types[j] == GA_BUFFER) {
+      strb_appendf(&sb, " %s_p,  const ga_size %s_offset",
+                   args->names[j], args->names[j]);
+    } else {
+      strb_appendf(&sb, " %s", args->names[j]);
+    }
+    if (j != (args->n - 1))
+      strb_appends(&sb, ", ");
+  }
+  strb_appends(&sb, ") {\n"
+               "const ga_size idx = LDIM_0 * GID_0 + LID_0;\n"
+               "const ga_size numThreads = LDIM_0 * GDIM_0;\n"
+               "ga_size i;\n"
+               "GLOBAL_MEM char *tmp;\n\n");
+  for (j = 0; j < args->n; j++) {
+    if (args->types[j] == GA_BUFFER) {
+      strb_appendf(&sb, "tmp = (GLOBAL_MEM char *)%s;"
+                   "tmp += %s_offset; %s = (",
+                   args->names[j], args->names[j], args->names[j]);
+      decl2(sb, args->types[j], args->args[j]);
+      strb_appends(&sb, ")tmp;\n");
+    }
+  }
+
+  strb_appends(&sb, "for (i = idx; i < n; i += numThreads) {\n");
+  for (j = 0; j < args->n; j++) {
+    if (args->types[j] == GA_BUFFER) {
+      decl2(sb, args->types[j], args->args[j]);
+      strb_appendf(&sb, " %s = &%s_p[i];", args->names[j], args->names[j]);
+    }
+  }
+  strb_appends(&sb, expr);
+  strb_appends(&sb, ";\n}\n}\n");
+
+  if (strb_error(&sb)) {
+    res = GA_MEMORY_ERROR;
+    goto bail;
+  }
+
+  res = GpuKernel_init(k, ops, ctx, 1, (const char **)&sb.s, &sb.l, "elem",
+                       args->n, args->types, flags, err_str);
+ bail:
+  strb_clear(&sb);
+  return res;
+}
+
+int gpuarray_elemwise_op(const gpuarray_buffer_ops *ops, void * ctx,
+                         const char *preamble, const char *expr,
+                         unsigned int n, const in *types, void **args,
+                         int flags, char **err_str) {
+  // TODO
 }
