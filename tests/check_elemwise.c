0e9063abc0a15801f03c75178e1edc24a6eda0b7
Arnaud Bergeron <abergeron@gmail.com>
2016-03-29 14:35:45 -0400

    Add test for the basic kernel too.

--- tests/check_elemwise.c
+++ tests/check_elemwise.c
@@ -65,7 +65,69 @@ START_TEST(test_contig_simple)
   rargs[1] = &b;
   rargs[2] = &c;
 
-  ga_assert_ok(GpuElemwise_call(ge, rargs, GE_BROADCAST));
+  ga_assert_ok(GpuElemwise_call(ge, rargs, GE_NOCOLLAPSE));
+
+  ga_assert_ok(GpuArray_read(data3, sizeof(data3), &c));
+
+  ck_assert_int_eq(data3[0], 5);
+  ck_assert_int_eq(data3[1], 7);
+  ck_assert_int_eq(data3[2], 9);
+}
+END_TEST
+
+
+START_TEST(test_basic_simple)
+{
+  GpuArray a;
+  GpuArray b;
+  GpuArray c;
+
+  GpuElemwise *ge;
+
+  static const uint32_t data1[3] = {1, 2, 3};
+  static const uint32_t data2[3] = {4, 5, 6};
+  uint32_t data3[3] = {0};
+
+  size_t dims[2];
+
+  gpuelemwise_arg args[3] = {{0}};
+  void *rargs[3];
+
+  dims[0] = 1;
+  dims[1] = 3;
+
+  ga_assert_ok(GpuArray_empty(&a, ops, ctx, GA_UINT, 2, dims, GA_C_ORDER));
+  ga_assert_ok(GpuArray_write(&a, data1, sizeof(data1)));
+
+  ga_assert_ok(GpuArray_empty(&b, ops, ctx, GA_UINT, 2, dims, GA_F_ORDER));
+  ga_assert_ok(GpuArray_write(&b, data2, sizeof(data2)));
+
+  ga_assert_ok(GpuArray_empty(&c, ops, ctx, GA_UINT, 2, dims, GA_C_ORDER));
+
+  args[0].name = "a";
+  args[0].nd = 2;
+  args[0].typecode = GA_UINT;
+  args[0].flags = GE_READ;
+
+  args[1].name = "b";
+  args[1].nd = 2;
+  args[1].typecode = GA_UINT;
+  args[1].flags = GE_READ;
+
+  args[2].name = "c";
+  args[2].nd = 2;
+  args[2].typecode = GA_UINT;
+  args[2].flags = GE_WRITE;
+
+  ge = GpuElemwise_new(ops, ctx, "", "c = a + b", 3, args, 0);
+
+  ck_assert_ptr_ne(ge, NULL);
+
+  rargs[0] = &a;
+  rargs[1] = &b;
+  rargs[2] = &c;
+
+  ga_assert_ok(GpuElemwise_call(ge, rargs, GE_NOCOLLAPSE));
 
   ga_assert_ok(GpuArray_read(data3, sizeof(data3), &c));
 
@@ -82,6 +144,8 @@ Suite *get_suite(void) {
   tcase_add_test(tc, test_contig_simple);
   suite_add_tcase(s, tc);
   tc = tcase_create("basic");
+  tcase_add_checked_fixture(tc, setup, teardown);
+  tcase_add_test(tc, test_basic_simple);
   suite_add_tcase(s, tc);
   return s;
 }
