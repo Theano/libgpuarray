8f63a606f7b4b8751092f141442fe548aee0b729
Arnaud Bergeron <abergeron@gmail.com>
2016-03-31 13:45:04 -0400

    Rewrite the FindCheck script so that it actually conforms to cmake standards and remove the hacks that are no longer necessary.

--- CMakeModules/FindCheck.cmake
+++ CMakeModules/FindCheck.cmake
@@ -1,55 +1,30 @@
-# - Try to find the CHECK libraries
-#  Once done this will define
+# This will find check and create the following variables
 #
-#  CHECK_FOUND - system has check
-#  CHECK_INCLUDE_DIRS - the check include directory
-#  CHECK_LIBRARIES - check library
-#  
-#  Copyright (c) 2007 Daniel Gollub <gollub@b1-systems.de>
-#  Copyright (c) 2007-2009 Bjoern Ricks  <bjoern.ricks@gmail.com>
+#  CHECK_FOUND
+#  CHECK_INCLUDE_DIR
+#  CHECK_LIBRARIES
+#  CHECK_DEFINITIONS
 #
-#  Redistribution and use is allowed according to the terms of the New
-#  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#  This is public domain. Reuse as you wish.
 
+find_package(PkgConfig)
 
-INCLUDE( FindPkgConfig )
+pkg_check_modules(PC_CHECK QUIET check)
 
-IF ( Check_FIND_REQUIRED )
-	SET( _pkgconfig_REQUIRED "REQUIRED" )
-ELSE( Check_FIND_REQUIRED )
-	SET( _pkgconfig_REQUIRED "" )	
-ENDIF ( Check_FIND_REQUIRED )
+set(CHECK_DEFINITIONS ${PC_CHECK_CFLAGS_OTHER})
 
-IF ( CHECK_MIN_VERSION )
-	PKG_SEARCH_MODULE( CHECK ${_pkgconfig_REQUIRED} check>=${CHECK_MIN_VERSION} )
-ELSE ( CHECK_MIN_VERSION )
-	PKG_SEARCH_MODULE( CHECK ${_pkgconfig_REQUIRED} check )
-ENDIF ( CHECK_MIN_VERSION )
+find_path(CHECK_INCLUDE_DIR check.h
+          HINTS ${PC_CHECK_INCLUDEDIR} ${PC_CHECK_INCLUDE_DIRS})
 
-# Look for CHECK include dir and libraries
-IF( NOT CHECK_FOUND AND NOT PKG_CONFIG_FOUND )
+find_library(CHECK_LIBRARY NAMES check
+             HINTS ${PC_CHECK_LIBRARY_DIRS} ${PC_CHECK_LIBDIR})
 
-	FIND_PATH( CHECK_INCLUDE_DIRS check.h )
+include(FindPackageHandleStandardArgs)
 
-	FIND_LIBRARY( CHECK_LIBRARIES NAMES check )
+find_package_handle_standard_args(Check DEFAULT_MSG
+                                  CHECK_LIBRARY CHECK_INCLUDE_DIR)
 
-	IF ( CHECK_INCLUDE_DIRS AND CHECK_LIBRARIES )
-		SET( CHECK_FOUND 1 )
-		IF ( NOT Check_FIND_QUIETLY )
-			MESSAGE ( STATUS "Found CHECK: ${CHECK_LIBRARIES}" )
-		ENDIF ( NOT Check_FIND_QUIETLY )
-	ELSE ( CHECK_INCLUDE_DIRS AND CHECK_LIBRARIES )
-		IF ( Check_FIND_REQUIRED )
-			MESSAGE( FATAL_ERROR "Could NOT find CHECK" )
-		ELSE ( Check_FIND_REQUIRED )
-			IF ( NOT Check_FIND_QUIETLY )
-				MESSAGE( STATUS "Could NOT find CHECK" )	
-			ENDIF ( NOT Check_FIND_QUIETLY )
-		ENDIF ( Check_FIND_REQUIRED )
-	ENDIF ( CHECK_INCLUDE_DIRS AND CHECK_LIBRARIES )
-ENDIF( NOT CHECK_FOUND AND NOT PKG_CONFIG_FOUND )
+mark_as_advanced(CHECK_INCLUDE_DIR CHECK_LIBRARY)
 
-# Hide advanced variables from CMake GUIs
-MARK_AS_ADVANCED( CHECK_INCLUDE_DIRS CHECK_LIBRARIES )
-
+set(CHECK_LIBRARIES ${CHECK_LIBRARY})
+set(CHECK_INCLUDE_DIRS ${CHECK_INCLUDE_DIR})
