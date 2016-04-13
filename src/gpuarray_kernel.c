f6e96557d361509b01b84f5184306106a69b10bc
Arnaud Bergeron <abergeron@gmail.com>
2016-04-13 15:28:53 -0400

    Use better scheduling for better occupancy.

--- src/gpuarray_kernel.c
+++ src/gpuarray_kernel.c
@@ -39,8 +39,12 @@ void *GpuKernel_context(GpuKernel *k) {
 int GpuKernel_sched(GpuKernel *k, size_t n, size_t *ls, size_t *gs) {
   size_t min_l;
   size_t max_l;
+  size_t target_l;
   size_t max_g;
+  size_t target_g;
+  unsigned int numprocs;
   int err;
+  int want_ls = 0;
 
   err = k->ops->property(NULL, NULL, k->k, GA_KERNEL_PROP_MAXLSIZE, &max_l);
   if (err != GA_NO_ERROR)
@@ -48,25 +52,38 @@ int GpuKernel_sched(GpuKernel *k, size_t n, size_t *ls, size_t *gs) {
   err = k->ops->property(NULL, NULL, k->k, GA_KERNEL_PROP_PREFLSIZE, &min_l);
   if (err != GA_NO_ERROR)
     return err;
-  err = k->ops->property(GpuKernel_context(k), NULL, NULL,
-                         GA_CTX_PROP_MAXGSIZE, &max_g);
+  err = k->ops->property(NULL, NULL, k->k, GA_CTX_PROP_NUMPROCS, &numprocs);
   if (err != GA_NO_ERROR)
     return err;
-  if (*gs == 0) {
-    if (*ls == 0) {
-      if (n < max_l)
-        *ls = min_l;
-      else
-        *ls = max_l;
-    }
-    *gs = ((n-1) / (*ls)) + 1;
-    if (*gs > max_g)
-      *gs = max_g;
-  } else if (*ls == 0) {
-    *ls = (n-1) / ((*gs)-1);
-    if (*ls > max_l)
-      *ls = max_l;
+  err = k->ops->property(NULL, NULL, k->k, GA_CTX_PROP_MAXGSIZE, &max_g);
+  if (err != GA_NO_ERROR)
+    return err;
+
+  /* Do something about these hardcoded values */
+  target_g = numprocs * 32;
+  if (target_g > max_g)
+    target_g = max_g;
+  target_l = 512;
+  if (target_l > max_l)
+    target_l = max_l;
+
+  if (*ls == 0) {
+    want_ls = 1;
+    *ls = min_l;
   }
+
+  if (*gs == 0) {
+    *gs = ((n-1) / *ls) + 1;
+    if (*gs > target_g)
+      *gs = target_g;
+  }
+
+  if (want_ls) {
+    *ls = (n-1) / ((*gs)-1);
+    if (*ls > target_l)
+      *ls = target_l;
+  }
+
   return GA_NO_ERROR;
 }
 
