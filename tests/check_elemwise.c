16074d91e09e0c8c9a0b5a45ba36986a5d6c4473
Arnaud Bergeron <abergeron@gmail.com>
2016-04-22 16:52:01 -0400

    Add a test with negative strides to (hopefully) catch the cuda upcast bug.

--- tests/check_elemwise.c
+++ tests/check_elemwise.c
@@ -327,6 +327,76 @@ START_TEST(test_basic_collapse)
 }
 END_TEST
 
+START_TEST(test_basic_neg_strides)
+{
+  GpuArray a;
+  GpuArray b;
+  GpuArray c;
+
+  GpuElemwise *ge;
+
+  static const uint32_t data1[6] = {1, 2, 3, 4, 5, 6};
+  static const uint32_t data2[6] = {7, 8, 9, 10, 11, 12};
+  uint32_t data3[6] = {0};
+
+  size_t dims[1];
+
+  gpuelemwise_arg args[3] = {{0}};
+  void *rargs[3];
+
+  ssize_t starts[1];
+  ssize_t stops[1];
+  ssize_t steps[1];
+
+  dims[0] = 6;
+
+  ga_assert_ok(GpuArray_empty(&a, ops, ctx, GA_UINT, 1, dims, GA_C_ORDER));
+  ga_assert_ok(GpuArray_write(&a, data1, sizeof(data1)));
+
+  ga_assert_ok(GpuArray_empty(&b, ops, ctx, GA_UINT, 1, dims, GA_C_ORDER));
+  ga_assert_ok(GpuArray_write(&b, data2, sizeof(data2)));
+
+  starts[0] = 5;
+  stops[0] = -1;
+  steps[0] = -1;
+
+  ga_assert_ok(GpuArray_index_inplace(&b, starts, stops, steps));
+
+  ga_assert_ok(GpuArray_empty(&c, ops, ctx, GA_UINT, 1, dims, GA_C_ORDER));
+
+  args[0].name = "a";
+  args[0].typecode = GA_UINT;
+  args[0].flags = GE_READ;
+
+  args[1].name = "b";
+  args[1].typecode = GA_UINT;
+  args[1].flags = GE_READ;
+
+  args[2].name = "c";
+  args[2].typecode = GA_UINT;
+  args[2].flags = GE_WRITE;
+
+  ge = GpuElemwise_new(ops, ctx, "", "c = a + b", 3, args, 1, 0);
+
+  ck_assert_ptr_ne(ge, NULL);
+
+  rargs[0] = &a;
+  rargs[1] = &b;
+  rargs[2] = &c;
+
+  ga_assert_ok(GpuElemwise_call(ge, rargs, 0));
+
+  ga_assert_ok(GpuArray_read(data3, sizeof(data3), &c));
+
+  ck_assert_int_eq(data3[0], 13);
+  ck_assert_int_eq(data3[1], 13);
+  ck_assert_int_eq(data3[2], 13);
+  ck_assert_int_eq(data3[3], 13);
+  ck_assert_int_eq(data3[4], 13);
+  ck_assert_int_eq(data3[5], 13);
+}
+END_TEST
+
 Suite *get_suite(void) {
   Suite *s = suite_create("elemwise");
   TCase *tc = tcase_create("contig");
@@ -339,6 +409,7 @@ Suite *get_suite(void) {
   tcase_add_test(tc, test_basic_remove1);
   tcase_add_test(tc, test_basic_broadcast);
   tcase_add_test(tc, test_basic_collapse);
+  tcase_add_test(tc, test_basic_neg_strides);
   suite_add_tcase(s, tc);
   return s;
 }
