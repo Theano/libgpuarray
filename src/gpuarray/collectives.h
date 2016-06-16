4d3ad15694b4f17c91f7638fb8f7461e8072fbe0
Christos Tsirigotis <tsirif@gmail.com>
2016-06-17 00:38:24 +0300

    Fix flag checking in array_collectives
    - Remove wrong GPUARRAY_PUBLIC in implementation
    - Lint

--- src/gpuarray/collectives.h
+++ src/gpuarray/collectives.h
@@ -80,15 +80,6 @@ GPUARRAY_PUBLIC int GpuArray_broadcast(GpuArray* array, int root, gpucomm* comm)
 GPUARRAY_PUBLIC int GpuArray_all_gather(const GpuArray* src, GpuArray* dest,
                                         gpucomm* comm);
 
-// TODO discuss GpuArray layout restrictions on collectives
-// 1. Reduce/AllReduce: consider as elemwise and check only total elem count? or
-//                      consider to be consistent in dim size and order too?
-// 2. Broadcast should probably consider consisent GpuArrays in everything.
-// 3. ReduceScatter: at least a restriction for 'total elems of src' = ndev * 'total
-// elems of dest'
-// 4. AllGather: at least a restriction for 'total elems of dest' = ndev * 'total
-// elems of src'
-
 #ifdef __cplusplus
 }
 #endif
--- src/gpuarray_array_collectives.c
+++ src/gpuarray_array_collectives.c
@@ -9,13 +9,12 @@ static inline int find_total_elems(const GpuArray* array)
 {
   unsigned int i;
   size_t total_elems = 1;
-  for (i = 0; i < array->nd; ++i)
-    total_elems *= array->dimensions[i];
-  return (int) total_elems;
+  for (i = 0; i < array->nd; ++i) total_elems *= array->dimensions[i];
+  return (int)total_elems;
 }
 
-static inline int check_gpuarrays(int times_src, const GpuArray* src,
-                                  int times_dest, const GpuArray* dest, int* count)
+static inline int check_gpuarrays(int times_src, const GpuArray* src, int times_dest,
+                                  const GpuArray* dest, int* count)
 {
   int count_src, count_dest;
   count_src = find_total_elems(src);
@@ -24,7 +23,7 @@ static inline int check_gpuarrays(int times_src, const GpuArray* src,
     return GA_VALUE_ERROR;
   if (src->typecode != dest->typecode)
     return GA_VALUE_ERROR;
-  if (!(src->flags & GA_ALIGNED) || !(dest->flags & GA_BEHAVED))
+  if (!GpuArray_ISALIGNED(src) || !GpuArray_CHKFLAGS(dest, GA_BEHAVED))
     return GA_UNALIGNED_ERROR;
 
   if (times_src >= times_dest)
@@ -36,40 +35,40 @@ static inline int check_gpuarrays(int times_src, const GpuArray* src,
 
 int GpuArray_reduce_from(const GpuArray* src, int opcode, int root, gpucomm* comm)
 {
-  if (!(src->flags & GA_ALIGNED))
+  if (!GpuArray_ISALIGNED(src))
     return GA_UNALIGNED_ERROR;
   int total_elems = find_total_elems(src);
-  return gpucomm_reduce(src->data, src->offset, NULL, 0,
-                        total_elems, src->typecode, opcode, root, comm);
+  return gpucomm_reduce(src->data, src->offset, NULL, 0, total_elems, src->typecode,
+                        opcode, root, comm);
 }
 
-int GpuArray_reduce(const GpuArray* src, GpuArray* dest, int opcode,
-                                    int root, gpucomm* comm)
+int GpuArray_reduce(const GpuArray* src, GpuArray* dest, int opcode, int root,
+                    gpucomm* comm)
 {
   int rank = 0;
   GA_CHECK(gpucomm_get_rank(comm, &rank));
   if (rank == root) {
     int count = 0;
     GA_CHECK(check_gpuarrays(1, src, 1, dest, &count));
-    return gpucomm_reduce(src->data, src->offset, dest->data, dest->offset,
-                          count, src->typecode, opcode, root, comm);
+    return gpucomm_reduce(src->data, src->offset, dest->data, dest->offset, count,
+                          src->typecode, opcode, root, comm);
   }
   else {
     return GpuArray_reduce_from(src, opcode, root, comm);
   }
 }
 
-int GpuArray_all_reduce(const GpuArray* src, GpuArray* dest,
-                                        int opcode, gpucomm* comm)
+int GpuArray_all_reduce(const GpuArray* src, GpuArray* dest, int opcode,
+                        gpucomm* comm)
 {
   int count = 0;
   GA_CHECK(check_gpuarrays(1, src, 1, dest, &count));
-  return gpucomm_all_reduce(src->data, src->offset, dest->data, dest->offset,
-                            count, src->typecode, opcode, comm);
+  return gpucomm_all_reduce(src->data, src->offset, dest->data, dest->offset, count,
+                            src->typecode, opcode, comm);
 }
 
-int GpuArray_reduce_scatter(const GpuArray* src, GpuArray* dest,
-                                            int opcode, gpucomm* comm)
+int GpuArray_reduce_scatter(const GpuArray* src, GpuArray* dest, int opcode,
+                            gpucomm* comm)
 {
   int count = 0, ndev = 0;
   GA_CHECK(gpucomm_get_count(comm, &ndev));
@@ -78,31 +77,30 @@ int GpuArray_reduce_scatter(const GpuArray* src, GpuArray* dest,
                                 count, src->typecode, opcode, comm);
 }
 
-GPUARRAY_PUBLIC int GpuArray_broadcast(GpuArray* array, int root, gpucomm* comm)
+int GpuArray_broadcast(GpuArray* array, int root, gpucomm* comm)
 {
   int rank = 0;
   GA_CHECK(gpucomm_get_rank(comm, &rank));
   if (rank == root) {
-    if (!(array->flags & GA_BEHAVED))
+    if (!GpuArray_CHKFLAGS(array, GA_BEHAVED))
       return GA_UNALIGNED_ERROR;
   }
   else {
-    if (!(array->flags & GA_ALIGNED))
+    if (!GpuArray_ISALIGNED(array))
       return GA_UNALIGNED_ERROR;
   }
 
   int total_elems = find_total_elems(array);
 
-  return gpucomm_broadcast(array->data, array->offset, total_elems,
-                           array->typecode, root, comm);
+  return gpucomm_broadcast(array->data, array->offset, total_elems, array->typecode,
+                           root, comm);
 }
 
-GPUARRAY_PUBLIC int GpuArray_all_gather(const GpuArray* src, GpuArray* dest,
-                                        gpucomm* comm)
+int GpuArray_all_gather(const GpuArray* src, GpuArray* dest, gpucomm* comm)
 {
   int count = 0, ndev = 0;
   GA_CHECK(gpucomm_get_count(comm, &ndev));
   GA_CHECK(check_gpuarrays(ndev, src, 1, dest, &count));
-  return gpucomm_all_gather(src->data, src->offset, dest->data, dest->offset,
-                            count, src->typecode, comm);
+  return gpucomm_all_gather(src->data, src->offset, dest->data, dest->offset, count,
+                            src->typecode, comm);
 }
