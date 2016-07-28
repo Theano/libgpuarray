078e5e122d6dd41dc8a7bf62543f471143887d19
Arnaud Bergeron <abergeron@gmail.com>
2016-07-28 17:23:35 -0400

    Change tests to match numpy 1.11 behaviour.

--- pygpu/operations.py
+++ pygpu/operations.py
@@ -5,14 +5,6 @@ from .dtypes import upcast
 from . import array, asarray
 
 
-def _replace_0_with_empty(aryl, m):
-    for i in range(len(aryl)):
-        if (len(aryl[i].shape) == 0 or
-                any(s == 0 for s in aryl[i].shape)):
-            aryl[i] = array([], dtype=m.dtype, cls=type(m), context=m.context)
-    return aryl
-
-
 def atleast_1d(*arys):
     res = []
     for ary in arys:
@@ -92,7 +84,7 @@ def array_split(ary, indices_or_sections, axis=0):
         divs = (list(range(neach + 1, (neach + 1) * extra + 1, neach + 1)) +
                 list(range((neach + 1) * extra + neach, ary.shape[axis], neach)))
         res = _split(ary, divs, axis)
-    return _replace_0_with_empty(res, ary)
+    return res
 
 
 def hsplit(ary, indices_or_sections):
