2782dd00205168fc27e8c7716113e373a8900ac5
Arnaud Bergeron <abergeron@gmail.com>
2016-11-14 14:08:53 -0500

    Add an implementation of float_to_half.

--- src/private.h
+++ src/private.h
@@ -264,6 +264,53 @@ GPUARRAY_LOCAL void gpukernel_source_with_line_numbers(unsigned int count,
                                                        size_t *newl,
                                                        strb *src);
 
+static uint16_t float_to_half(float value) {
+  static const int shift = 13;
+  static const int shiftSign = 16;
+
+  static const int32_t infN = 0x7F800000; // flt32 infinity
+  static const int32_t maxN = 0x477FE000; // max flt16 normal as a flt32
+  static const int32_t minN = 0x38800000; // min flt16 normal as a flt32
+  static const int32_t signN = 0x80000000; // flt32 sign bit
+
+  static const int32_t infC = infN >> shift;
+  static const int32_t nanN = (infC + 1) << shift; // minimum flt16 nan as a flt32
+  static const int32_t maxC = maxN >> shift;
+  static const int32_t minC = minN >> shift;
+  static const int32_t signC = signN >> shiftSign; // flt16 sign bit
+
+  static const int32_t mulN = 0x52000000; // (1 << 23) / minN
+  static const int32_t mulC = 0x33800000; // minN / (1 << (23 - shift))
+
+  static const int32_t subC = 0x003FF; // max flt32 subnormal down shifted
+  static const int32_t norC = 0x00400; // min flt32 normal down shifted
+
+  static const int32_t maxD = infC - maxC - 1;
+  static const int32_t minD = minC - subC - 1;
+
+  union {
+    float f;
+    int32_t si;
+    uint32_t ui;
+  } v, s;
+
+  uint32_t sign;
+
+  v.f = value;
+  sign = v.si & signN;
+  v.si ^= sign;
+  sign >>= shiftSign; // logical shift
+  s.si = mulN;
+  s.si = s.f * v.f; // correct subnormals
+  v.si ^= (s.si ^ v.si) & -(minN > v.si);
+  v.si ^= (infN ^ v.si) & -((infN > v.si) & (v.si > maxN));
+  v.si ^= (nanN ^ v.si) & -((nanN > v.si) & (v.si > infN));
+  v.ui >>= shift; // logical shift
+  v.si ^= ((v.si - maxD) ^ v.si) & -(v.si > maxC);
+  v.si ^= ((v.si - minD) ^ v.si) & -(v.si > subC);
+  return v.ui | sign;
+}
+
 #define ISSET(v, fl) ((v) & (fl))
 #define ISCLR(v, fl) (!((v) & (fl)))
 
