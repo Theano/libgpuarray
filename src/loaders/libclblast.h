bbd54a80f62b5f56e33d9aa1137c2ee87947bd4e
Arnaud Bergeron <abergeron@gmail.com>
2017-03-24 16:12:48 -0400

    Add error code for clblast.

--- src/loaders/libclblast.h
+++ src/loaders/libclblast.h
@@ -15,10 +15,36 @@ typedef enum Transpose_ {
   kConjugate = 113
 } Transpose;
 
-typedef enum StatusCode_ {
+typedef enum CLBLastStatusCode_ {
   kSuccess = 0,
   /* Rest is not exposed from here */
-} StatusCode;
+  CLBlastNotImplemented            = -1024,
+  CLBlastInvalidMatrixA            = -1022,
+  CLBlastInvalidMatrixB            = -1021,
+  CLBlastInvalidMatrixC            = -1020,
+  CLBlastInvalidVectorX            = -1019,
+  CLBlastInvalidVectorY            = -1018,
+  CLBlastInvalidDimension          = -1017,
+  CLBlastInvalidLeadDimA           = -1016,
+  CLBlastInvalidLeadDimB           = -1015,
+  CLBlastInvalidLeadDimC           = -1014,
+  CLBlastInvalidIncrementX         = -1013,
+  CLBlastInvalidIncrementY         = -1012,
+  CLBlastInsufficientMemoryA       = -1011,
+  CLBlastInsufficientMemoryB       = -1010,
+  CLBlastInsufficientMemoryC       = -1009,
+  CLBlastInsufficientMemoryX       = -1008,
+  CLBlastInsufficientMemoryY       = -1007,
+
+  CLBlastInvalidLocalMemUsage      = -2046,
+  CLBlastNoHalfPrecision           = -2045,
+  CLBlastNoDoublePrecision         = -2044,
+  CLBlastInvalidVectorScalar       = -2043,
+  CLBlastInsufficientMemoryScalar  = -2042,
+  CLBlastDatabaseError             = -2041,
+  CLBlastUnknownError              = -2040,
+  CLBlastUnexpectedError           = -2039,
+} CLBlastStatusCode;
 
 int load_libclblast(error *);
 
