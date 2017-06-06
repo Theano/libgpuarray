885129241dde64d422635518e89eea32cc8716cc
Arnaud Bergeron <abergeron@gmail.com>
2017-06-06 16:22:00 -0400

    Errors all around.

--- src/gpuarray_buffer.c
+++ src/gpuarray_buffer.c
@@ -6,6 +6,7 @@
 #include "gpuarray/buffer_collectives.h"
 #include "gpuarray/error.h"
 
+#include "util/error.h"
 #include "private.h"
 
 extern const gpuarray_buffer_ops cuda_ops;
@@ -22,7 +23,7 @@ const gpuarray_buffer_ops *gpuarray_get_ops(const char *name) {
 int gpu_get_platform_count(const char* name, unsigned int* platcount) {
   const gpuarray_buffer_ops* ops = gpuarray_get_ops(name);
   if (ops == NULL) {
-    return GA_INVALID_ERROR;
+    return error_set(&global_err, GA_INVALID_ERROR, "Invalid platform");
   }
   return ops->get_platform_count(platcount);
 }
@@ -31,7 +32,7 @@ int gpu_get_device_count(const char* name, unsigned int platform,
                          unsigned int* devcount) {
   const gpuarray_buffer_ops* ops = gpuarray_get_ops(name);
   if (ops == NULL) {
-    return GA_INVALID_ERROR;
+    return error_set(&global_err, GA_INVALID_ERROR, "Invalid platform");
   }
   return ops->get_device_count(platform, devcount);
 }
@@ -121,7 +122,7 @@ int gpudata_transfer(gpudata *dst, size_t dstoff, gpudata *src, size_t srcoff,
   /* Fallback to host copy */
   tmp = malloc(sz);
   if (tmp == NULL)
-    return GA_MEMORY_ERROR;
+    return error_sys(ctx->err, "malloc");
   res = src_ctx->ops->buffer_read(tmp, src, srcoff, sz);
   if (res != GA_NO_ERROR) {
     free(tmp);
