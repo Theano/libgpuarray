82f7e9785ff4e76555b3fdd35aeb23c4245586e9
vcampmany <vcampmany@gmail.com>
2017-07-12 22:43:53 +0200

    fist commit

---
+++ src/gpuarray_sort.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+#include "util/strb.h"
+
+
+int GpuArray_sort(GpuArray *r, GpuArray *a, unsigned int numaxes, unsigned int *axes, GpuArray *arg)
+{
+	if (arg != NULL)
+  {
+    // perform argsort
+  }
+  else
+  {
+    // perform regular sort
+  }
+
+}
