7c9999ddb6590a6478e039c4dbd72af7caceea16
Simon Lefrancois <lefransi@iro.umontreal.ca>
2017-07-27 16:09:29 -0400

    correct win pr

--- .jenkins_pr_win.bat
+++ .jenkins_pr_win.bat
@@ -1,61 +1,43 @@
-REM Set path for conda python and cmake
-set PATH=%PATH%;C:\ProgramData\Miniconda2;C:\Program Files\CMake\bin
+REM Set path for cuda, conda python and cmake
+set PATH=%PATH%;C:\ProgramData\Miniconda2;C:\Program Files\CMake\bin;C:\lib\cuda\bin
 
-# Can also set to "Debug", "Release" to go faster
+REM Can also set to "Debug", "Release" to go faster
 set GPUARRAY_CONFIG="Release"
-# Set these to " " to disable (empty doesn't work)
-set DEVICES_CUDA="cuda" # for multiple devices use "cuda0 cuda1"
-set DEVICES_OPENCL=""
+REM Use spaces to seperate devices
+set DEVICES_CUDA=cuda
+set DEVICES_OPENCL=
 
 git rev-parse HEAD
 
-# Build libgpuarray and run C tests
+REM Clean up previous installs (to make sure no old files are left)
+rmdir %WORKSPACE%\lib /s/q
+mkdir %WORKSPACE%\lib
+rmdir build /s/q
 mkdir build
+
+REM Build libgpuarray and run C tests
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=%GPUARRAY_CONFIG% -G "NMake Makefiles"
 nmake
 cd ..
 
-REM Export paths
-set LIBDIR=%WORKSPACE%\local
-set PATH=%PATH%;%LIBDIR%\lib;C:\lib\cuda\bin
+set PATH=%PATH%;%WORKSPACE%\lib
 
-REM Clean up previous installs (to make sure no old files are left)
-rmdir %LIBDIR% /s/q
-mkdir %LIBDIR%
-
-# Test on different devices
-(for %%dev in (%DEVICES_CUDA%) do (
-    echo "Testing libgpuarray for DEVICE=%%dev"
-    cd build
-    set DEVICE=%%dev
-    make test
-    cd ..
-))
-
-(for %%dev in (%DEVICES_OPENCL%) do (
-    echo "Testing libgpuarray for DEVICE=%%dev"
-    cd build
-    set DEVICE=%%dev
-    make test
-    cd ..
-))
-
-REM Set conda python path
-set PATH=%PATH%;C:\ProgramData\Miniconda2;C:\ProgramData\Miniconda2\Library\mingw-w64\bin;C:\ProgramData\Miniconda2\Library\usr\bin;C:\ProgramData\Miniconda2\Library\bin;C:\ProgramData\Miniconda2\Scripts
+REM Add conda gcc toolchain path
+set PATH=%PATH%;C:\ProgramData\Miniconda2\Library\mingw-w64\bin;C:\ProgramData\Miniconda2\Library\usr\bin;C:\ProgramData\Miniconda2\Library\bin;C:\ProgramData\Miniconda2\Scripts
 
 REM Build the pygpu modules
 python setup.py build_ext --inplace
 
-# Test it
+REM Test pygpu
 set test=pygpu
-(for %%dev in (%DEVICES_CUDA%) do (
-    echo "Testing pygpu for DEVICE=%%dev"
-    set DEVICE=%%dev
-    nosetests --with-xunit --xunit-file=%test%_%DEVICE%_tests.xml pygpu\tests
-))
-(for %%dev in (%DEVICES_OPENCL%) do (
-    echo "Testing pygpu for DEVICE=%%dev"
-    set DEVICE=%%dev
-    nosetests --with-xunit --xunit-file=%test%_%DEVICE%_tests.xml pygpu\tests
-))
+for %%d in (%DEVICES_CUDA%) do (
+    echo "Testing pygpu for DEVICE=%%d"
+    set DEVICE=%%d
+	nosetests --with-xunit --xunit-file=%test%_%DEVICE%_tests.xml pygpu\tests
+)
+for %%d in (%DEVICES_OPENCL%) do (
+    echo "Testing pygpu for DEVICE=%%d"
+    set DEVICE=%%d
+    nosetests --with-xunit --xunit-file=%test%_%DEVICE%_tests.xml pygpu\tests -e test_blas.py
+)
