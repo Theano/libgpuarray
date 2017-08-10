aaa28b81ccf8676860fcaa05a64df79b57bd9318
Arnaud Bergeron <abergeron@gmail.com>
2017-08-10 11:38:29 -0400

    Unifiy the API for atomics and provide for all types 32 bits and up along with half.

--- src/cluda_cuda.h
+++ src/cluda_cuda.h
@@ -8,6 +8,7 @@
 #undef NAN
 #endif
 #define NAN __int_as_float(0x7fffffff)
+/* NULL */
 #ifdef INFINITY
 #undef INFINITY
 #endif
@@ -46,3 +47,103 @@
 struct ga_half {
   ga_ushort data;
 };
+
+#define gen_atom_add(name, argtype, wtype)              \
+  __device__ argtype name(argtype *addr, argtype val) { \
+    union {                                             \
+      argtype a;                                        \
+      wtype w;                                          \
+    } p, n;                                             \
+    p.a = *addr;                                        \
+    do {                                                \
+      n.a = p.a + val;                                  \
+      p.w = atomicCAS((wtype *)addr, p.w, n.w);         \
+    } while (p.w != n.w);                               \
+    return n.a;                                         \
+  }
+
+#define gen_atom32_add(name, argtype) gen_atom_add(name, argtype, unsigned int)
+#define gen_atom64_add(name, argtype) gen_atom_add(name, argtype, unsigned long long)
+
+#define gen_atom_xchg(name, argtype, wtype)              \
+  __device__ argtype name(argtype *addr, argtype val) { \
+    union {                                             \
+      argtype a;                                        \
+      wtype w;                                          \
+    } n, p;                                             \
+    n.a = val;                                          \
+    p.w = atomicExch((wtype *)addr, n.w);               \
+    return p.a;                                         \
+  }
+
+#define gen_atom32_xchg(name, argtype) gen_atom_xhg(name, argtype, unsigned int)
+#define gen_atom64_xchg(name, argtype) gen_atom_xhg(name, argtype, unsigned long long)
+
+/* ga_int */
+#define atom_add_ig(a, b, c) atomicAdd(a, b, c)
+#define atom_add_il(a, b, c) atomicAdd(a, b, c)
+#define atom_xchg_ig(a, b) atomicExch(a, b)
+#define atom_xchg_il(a, b) atomicExch(a, b)
+/* ga_uint */
+#define atom_add_Ig(a, b, c) atomicAdd(a, b, c)
+#define atom_add_Il(a, b, c) atomicAdd(a, b, c)
+#define atom_xchg_Ig(a, b) atomicExch(a, b)
+#define atom_xchg_Il(a, b) atomicExch(a, b)
+/* ga_long */
+gen_atom64_add(atom_add_lg, ga_long)
+#define atom_add_ll(a, b) atom_add_lg(a, b)
+gen_atom64_xhg(atom_xchg_lg, ga_long)
+#define atom_xchg_ll(a, b) atom_xchg_lg(a, b)
+/* ga_ulong */
+#define atom_add_Lg(a, b, c) atomicAdd(a, b, c)
+#define atom_add_Ll(a, b, c) atomicAdd(a, b, c)
+#define atom_xchg_Lg(a, b) atomicExch(a, b)
+#define atom_xchg_Ll(a, b) atomicExch(a, b)
+/* ga_float */
+#define atom_add_fg(a, b, c) atomicAdd(a, b, c)
+#define atom_add_fl(a, b, c) atomicAdd(a, b, c)
+#define atom_xchg_fg(a, b) atomicExch(a, b)
+#define atom_xchg_fl(a, b) atomicExch(a, b)
+/* ga_double */
+#if __CUDA_ARCH__ < 600
+gen_atom64_add(atom_add_dg, ga_double)
+#define atom_add_dl(a, b) atom_add_dg(a, b)
+#else
+#define atom_add_dg(a, b, c) atomicAdd(a, b, c)
+#define atom_add_dl(a, b, c) atomicAdd(a, b, c)
+#endif
+gen_atom64_xchg(atom_xchg_dg, ga_double)
+#define atom_xchg_dl(a, b) atom_xchg_dg(a, b)
+#endif
+/* ga_half */
+__device__ ga_half atom_add_hg(ga_half *addr, ga_half val) {
+  ga_uint *base = (ga_uint *)((ga_size)addr & ~2);
+  ga_uint old, assumed, sum, new_;
+  old = *base;
+  do {
+    assumed = old;
+    sum = __float2half_rn(
+                          __half2float(val) +
+                          __half2float((ga_half)__byte_perm(old, 0,
+                                                            ((ga_size)addr & 2) ? 0x4432 : 0x4410)));
+    new_ = __byte_perm(old, sum, ((ga_size)addr & 2) ? 0x5410 : 0x3254);
+    old = atomicCAS(base, assumed, new_);
+  } while (assumed != old);
+  return (ga_half)__byte_perm(old, 0,
+                              ((ga_size)addr & 2) ? 0x4432 : 0x4410);
+}
+#define atom_add_hl(a, b) atom_add_hg(a, b)
+
+__device__ ga_half atom_xchg_hg(ga_half *addr, ga_half val) {
+  ga_uint *base = (ga_uint *)((ga_size)addr & ~2);
+  ga_uint old, assumed, new_;
+  old = *base;
+  do {
+    assumed = old;
+    new_ = __byte_perm(old, val, ((ga_size)addr & 2) ? 0x5410 : 0x3254);
+    old = atomicCAS(base, assumed, new_);
+  } while (assumed != old);
+  return (ga_half)__byte_perm(old, 0,
+                              ((ga_size)addr & 2) ? 0x4432 : 0x4410);
+}
+#define atom_xchg_hl(a, b) atom_xchg_hg(a, b)
--- src/cluda_opencl.h
+++ src/cluda_opencl.h
@@ -4,9 +4,11 @@
 #define GLOBAL_MEM __global
 #define LOCAL_MEM __local
 #define LOCAL_MEM_ARG __local
+/* NAN */
 #ifndef NULL
   #define NULL ((void*)0)
 #endif
+/* INFINITY */
 #define LID_0 get_local_id(0)
 #define LID_1 get_local_id(1)
 #define LID_2 get_local_id(2)
@@ -41,3 +43,137 @@
 struct ga_half {
   half data;
 };
+
+#pragma OPENCL_EXTENSION cl_khr_int64_base_atomics: enable
+
+#define gen_atom32_add(name, argtype, aspace)                     \
+  argtype name(volatile aspace argtype *addr, argtype val) {      \
+    union {                                                       \
+      argtype a;                                                  \
+      int w;                                                      \
+    } p, n;                                                       \
+    int a;                                                        \
+    p.a = *addr;                                                  \
+    do {                                                          \
+      a = p.w;                                                    \
+      n.a = p.a + val;                                            \
+      p.w = atomic_cmpxhg((volatile aspace int *)addr, a, n.w);   \
+    } while (p.w != a);                                           \
+    return n.a;                                                   \
+  }
+
+#define gen_atom64_add(name, argtype, aspace)                     \
+  argtype name(volatile aspace argtype *addr, argtype val) {      \
+    union {                                                       \
+      argtype a;                                                  \
+      long w;                                                     \
+    } p, n;                                                       \
+    long a;                                                       \
+    p.a = *addr;                                                  \
+    do {                                                          \
+      a = p.w;                                                    \
+      n.a = p.a + val;                                            \
+      p.w = atom_cmpxhg((volatile aspace long *)addr, a, n.w);    \
+    } while (p.w != a);                                           \
+    return n.a;                                                   \
+  }
+
+#define gen_atom64_xchg(name, argtype, aspace)                  \
+  argtype name(volatile aspace argtype *addr, argtype val) {    \
+    union {                                                     \
+      argtype a;                                                \
+      long w;                                                   \
+    } p, n;                                                     \
+    n.a = val;                                                  \
+    p.w = atom_xchg((volatile aspace wtype *)addr, n.w);        \
+    return p.a;                                                 \
+  }
+
+/* ga_int */
+#define atom_add_ig(a, b) atomic_add(a, b)
+#define atom_add_il(a, b) atomic_add(a, b)
+#define atom_xchg_ig(a, b) atomic_xchg(a, b)
+#define atom_xchg_il(a, b) atomic_xchg(a, b)
+/* ga_uint */
+#define atom_add_Ig(a, b) atomic_add(a, b)
+#define atom_add_Il(a, b) atomic_add(a, b)
+#define atom_xchg_Ig(a, b) atomic_xchg(a, b)
+#define atom_xchg_Il(a, b) atomic_xchg(a, b)
+/* ga_long */
+#define atom_add_lg(a, b) atom_add(a, b)
+#define atom_add_ll(a, b) atom_add(a, b)
+#define atom_xchg_lg(a, b) atom_xchg(a, b)
+#define atom_xchg_ll(a, b) atom_xchg(a, b)
+/* ga_ulong */
+#define atom_add_Lg(a, b) atom_add(a, b)
+#define atom_add_Ll(a, b) atom_add(a, b)
+#define atom_xchg_Lg(a, b) atom_xchg(a, b)
+#define atom_xchg_Ll(a, b) atom_xchg(a, b)
+/* ga_float */
+gen_atom32_add(atom_add_fg, ga_float, global)
+gen_atom32_add(atom_add_fl, ga_float, local)
+#define atom_xchg_fg(a, b) atomic_xchg(a, b)
+#define atom_xchg_fl(a, b) atomic_xchg(a, b)
+/* ga_double */
+gen_atom64_add(atom_add_dg, ga_double, global)
+gen_atom64_add(atom_add_dl, ga_double, local)
+gen_atom64_xchg(atom_xchg_dg, ga_double, global)
+gen_atom64_xchg(atom_xchg_dl, ga_double, local)
+/* ga_half */
+#define gen_atomh_add(name, aspace) \
+  ga_half name(volatile aspace ga_half *addr, ga_half val) {            \
+    ga_size off = (ga_size)addr & 2;                                    \
+    volatile aspace int *base = (volatile aspace int *)((ga_size)addr - off); \
+    int o, a, n;                                                        \
+    float fo;                                                           \
+    float fval;                                                         \
+    ga_half hn;                                                         \
+    fval = vload_half(0, &val->data);                                   \
+    o = *base;                                                          \
+    do {                                                                \
+      a = o;                                                            \
+      /* This loads the half of `o` that we want to update */           \
+      fo = vload_half(off, (__private half *)&o);                       \
+      /* We compute the half addition in float 32 */                    \
+      store_half(fval + fo, &hn);                                       \
+      /* Now we reassemble the the parts to form a 32-bits n */         \
+      if (off == 2)                                                     \
+        n = (int)hn->data << 16 & (o & 0xffff);                         \
+      else                                                              \
+        n = (int)hn->data & (o & 0xffff0000);                           \
+      o = atomic_cmpxchg(base, a, n);                                   \
+    } while (o != a);                                                   \
+    if (off == 2)                                                       \
+      hn->data = (ushort)(o >> 16);                                     \
+    else                                                                \
+      hn->data = (ushort)(o & 0xffff);                                  \
+    return hn;                                                          \
+  }
+
+#define gen_atomh_xchg(name, aspace) \
+  ga_half name(volatile aspace ga_half *addr, ga_half *val) { \
+    ga_size off = (ga_size)addr & 2;                                    \
+    volatile aspace int *base = (volatile aspace int *)((ga_size)addr - off); \
+    int o, a, n;                                                        \
+    ga_half hr;                                                         \
+    o = *base;                                                          \
+    do {                                                                \
+      a = o;                                                            \
+      /* we have to combine our half value with the right part of `o` */ \
+      if (off == 2)                                                     \
+        n = (int)val->data << 16 & (o & 0xffff);                        \
+      else                                                              \
+        n = (int)val->data & (o & 0xffff0000);                          \
+      o = atomic_cmpxchg(base, a, n);                                   \
+    } while (o != a);                                                   \
+    if (off == 2)                                                       \
+      hr->data = (ushort)o << 16;                                       \
+    else                                                                \
+      hr->data = (ushort)o & 0xffff;                                    \
+    return hr;                                                          \
+  }
+
+gen_atomh_add(atom_add_hg, global)
+gen_atomh_add(atom_add_hl, local)
+gen_atomh_xchg(atom_xchg_hg, global)
+gen_atomh_xchg(atom_xchg_hl, local)
