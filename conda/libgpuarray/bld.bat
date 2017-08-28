be5ccb5886b87b44f8c27907a547db644348c5dd
Arnaud Bergeron <abergeron@gmail.com>
2017-08-28 13:56:14 -0400

    Improve the package scripts.

---
+++ conda/libgpuarray/bld.bat
@@ -0,0 +1,8 @@
+cmake -G"NMake Makefiles" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_C_FLAGS="-I%LIBRARY_PREFIX%\include" ^
+      "%SRC_DIR%"
+cmake --build . --config Release --target ALL_BUILD
+cmake --build . --config Release --target install
--- conda/libgpuarray/build.sh
+++ conda/libgpuarray/build.sh
@@ -1,2 +1,9 @@
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
-make install
+#!/bin/bash
+
+if [[ $(uname) == Darwin ]]; then
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_OSX_DEPLOYMENT_TARGET=
+else
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
+fi
+cmake --build . --config Release --target all
+cmake --build . --config Release --target install
