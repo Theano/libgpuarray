388896d8312420196e9062cc288cba70ad015135
Arnaud Bergeron <abergeron@gmail.com>
2017-09-01 12:55:22 -0400

    Fix bld.bat

--- conda/libgpuarray/bld.bat
+++ conda/libgpuarray/bld.bat
@@ -7,7 +7,5 @@ cmake -G"NMake Makefiles" ^
       -DCMAKE_C_FLAGS="-I%LIBRARY_PREFIX%\include" ^
       "%SRC_DIR%"
 if errorlevel 1 exit 1
-cmake --build . --config Release --target ALL_BUILD
-if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
