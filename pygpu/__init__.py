1b2aa4cab3ccde2849c2c962cdd7a1def70ea1be
Arnaud Bergeron <abergeron@gmail.com>
2017-09-01 13:42:49 -0400

    Add versioneer for better versions.

--- pygpu/__init__.py
+++ pygpu/__init__.py
@@ -12,11 +12,13 @@ from .operations import (split, array_split, hsplit, vsplit, dsplit,
                          concatenate, hstack, vstack, dstack)
 from ._array import ndgpuarray
 
-from .version import fullversion as __version__
-
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 def test():
     from . import tests
     from .tests import main
     if hasattr(main, "NoseTester"):
         main.NoseTester(package=tests).test()
+
