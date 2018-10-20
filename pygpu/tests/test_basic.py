07cd4ad56054c279442ee28413b26939f4c03632
Wong Hang <wonghang@gmail.com>
2018-10-20 22:02:26 +0800

    add float64 support to tril triu

--- pygpu/tests/test_basic.py
+++ pygpu/tests/test_basic.py
@@ -7,31 +7,33 @@ import numpy
 
 
 def test_tril():
-    for shape in [(10, 5), (5, 10), (10, 10)]:
-        for order in ['c', 'f']:
-            for inplace in [True, False]:
-                ac, ag = gen_gpuarray(shape, 'float32',
-                                      order=order, ctx=context)
-                result = tril(ag, inplace=inplace)
-                assert numpy.all(numpy.tril(ac) == result)
-                if inplace:
-                    assert numpy.all(numpy.tril(ac) == ag)
-                else:
-                    assert numpy.all(ac == ag)
+    for dtype in ['float32','float64']:
+        for shape in [(10, 5), (5, 10), (10, 10)]:
+            for order in ['c', 'f']:
+                for inplace in [True, False]:
+                    ac, ag = gen_gpuarray(shape, dtype,
+                                          order=order, ctx=context)
+                    result = tril(ag, inplace=inplace)
+                    assert numpy.all(numpy.tril(ac) == result)
+                    if inplace:
+                        assert numpy.all(numpy.tril(ac) == ag)
+                    else:
+                        assert numpy.all(ac == ag)
 
 
 def test_triu():
-    for shape in [(10, 5), (5, 10), (10, 10)]:
-        for order in ['c', 'f']:
-            for inplace in [True, False]:
-                ac, ag = gen_gpuarray(shape, 'float32',
-                                      order=order, ctx=context)
-                result = triu(ag, inplace=inplace)
-                assert numpy.all(numpy.triu(ac) == result)
-                if inplace:
-                    assert numpy.all(numpy.triu(ac) == ag)
-                else:
-                    assert numpy.all(ac == ag)
+    for dtype in ['float32','float64']:
+        for shape in [(10, 5), (5, 10), (10, 10)]:
+            for order in ['c', 'f']:
+                for inplace in [True, False]:
+                    ac, ag = gen_gpuarray(shape, dtype,
+                                          order=order, ctx=context)
+                    result = triu(ag, inplace=inplace)
+                    assert numpy.all(numpy.triu(ac) == result)
+                    if inplace:
+                        assert numpy.all(numpy.triu(ac) == ag)
+                    else:
+                        assert numpy.all(ac == ag)
 
 
 class test_errors(TestCase):
